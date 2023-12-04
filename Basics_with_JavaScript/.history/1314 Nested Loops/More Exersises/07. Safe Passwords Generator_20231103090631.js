b86b6bbbda9d6da46128e975c193107daab0fa02
rangerdio <148572548+rangerdio@users.noreply.github.com>
2023-12-04 19:36:32 +0200

    removing .history

--- Basics_with_JavaScript/.history/1314 Nested Loops/More Exersises/07. Safe Passwords Generator_20231103090631.js
+++
@@ -1,9 +0,0 @@
-function vladi(array){
-
-    
-
-    
-}
-
-vladi(["2","3","10"]);
-vladi(["20","50","10"]);
--- Basics_with_JavaScript/.history/1314 Nested Loops/More Exersises/07. Safe Passwords Generator_20231103090822.js
+++
@@ -1,9 +0,0 @@
-function vladi(array){
-
-    let a = Number(array)
-
-    
-}
-
-vladi(["2","3","10"]);
-vladi(["20","50","10"]);
