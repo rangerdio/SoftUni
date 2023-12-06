61a4a0084cf3eebc1da6f2459a003675387fd216
rangerdio <148572548+rangerdio@users.noreply.github.com>
2023-12-06 13:56:07 +0200

    invert_values

---
+++ Fundamentals_with_Python/Pre/current/11-12 Lists Basics/12 Exercise/00. Python_List_Methods.py
@@ -0,0 +1,48 @@
+num_list = [1, 2, 5, 1, 6, 8, 10, 1, 1, 4]
+str_list = ["aaa", "bbb", "AaA", "aAa", "aaA", "AAa", "ttt", "BBB", "aaa", "vvv"]
+mix_list = [1, "AaA", "aaa", 1, "aaA", 5, "bbb", 2, "aAa", 6]
+
+# num_list.append("Asd")
+# num_list.clear()
+# new_num_list = num_list.copy()
+# count = str_list.count("aaa")
+# num_list.extend(str_list)         can not be inside print()
+# print(str_list.index("aaa"))
+# num_list.insert(3, "Ivan")
+# print(num_list.pop(5))
+# num_list.remove(8)
+# str_list.remove("bbb")
+# str_list.reverse()
+# num_list.sort()
+# str_list.sort(reverse=True)
+
+print(num_list)
+print(str_list)
+
+
+"""
+Method	        Description
+append()	    Adds an element at the end of the list
+clear()	        Removes all the elements from the list
+copy()	        Returns a copy of the list
+count()	        Returns the number of elements with the specified value
+extend()	    Add the elements of a list (or any iterable), to the end of the current list
+index()	        Returns the index of the first element with the specified value
+insert()	    Adds an element at the specified position
+pop()	        Removes the element at the specified position
+remove()	    Removes the first item with the specified value
+reverse()	    Reverses the order of the list
+sort()	        Sorts the list
+clear()	        Removes all the elements from the dictionary
+copy()	        Returns a copy of the dictionary
+fromkeys()	    Returns a dictionary with the specified keys and value
+get()	        Returns the value of the specified key
+items()	        Returns a list containing a tuple for each key value pair
+keys()	        Returns a list containing the dictionary's keys
+pop()	        Removes the element with the specified key
+popitem()	    Removes the last inserted key-value pair
+setdefault()	Returns the value of the specified key. If the key not exist: insert the key, with the specified value
+update()	    Updates the dictionary with the specified key-value pairs
+values()	    Returns a list of all the values in the dictionary
+
+"""
