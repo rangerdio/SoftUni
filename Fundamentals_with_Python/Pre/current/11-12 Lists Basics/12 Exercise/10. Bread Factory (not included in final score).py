aa4dc6e63b936dd0a4b32614a6957203f092bed0
rangerdio <148572548+rangerdio@users.noreply.github.com>
2023-12-10 20:38:13 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/11-12 Lists Basics/12 Exercise/10. Bread Factory (not included in final score).py
+++ Fundamentals_with_Python/Pre/current/11-12 Lists Basics/12 Exercise/10. Bread Factory (not included in final score).py
@@ -5,45 +5,38 @@ all_events_handled = True
 
 for element in range(len(working_day)):
     current_element = working_day[element].split("-")
-    # current_element[1] = int(current_element[1])
+    event = current_element[0]
+    points = int(current_element[1])
 
-    if current_element[0] == "rest":
-        element_energy = int(current_element[1])
-        if current_energy == 100:
-            print("You gained 0 energy.")
-            print(f"Current energy: {current_energy}.")
-        elif current_energy + element_energy <= 100:
-            current_energy += element_energy
-            print(f"You gained {element_energy} energy.")
-            print(f"Current energy: {current_energy}.")
-
-        elif (current_energy + element_energy) > 100 > current_energy:
-            cur = current_energy
+    if event == "rest":
+        energy_var = current_energy
+        current_energy += points
+        if current_energy > 100:
             current_energy = 100
-            print(f"You gained {100 - cur} energy.")
-            print(f"Current energy: {current_energy}.")
-
-    elif current_element[0] == "order":
-        earned = int(current_element[1])
-        if current_energy >= 30:
-            coins += earned
-            current_energy -= 30
-            print(f"You earned {earned} coins.")
+            print(f"You gained {100 - energy_var} energy.")
         else:
-            # skipped the order
-            earned = 0
+            print(f"You gained {points} energy.")
+        print(f"Current energy: {current_energy}.")
+
+    elif event == "order":
+        if current_energy >= 30:
+            print(f"You earned {points} coins.")
+            coins += points
+            current_energy -= 30
+        else:
             current_energy += 50
             print("You had to rest!")
-            all_events_handled = False
+            # all_events_handled = False    #judge wants it commented! It is per the request:
     else:
-        ingredient = current_element[0]
-        if coins >= int(current_element[1]):
+        ingredient = event
+        if coins >= points:
             print(f"You bought {ingredient}.")
-            coins -= int(current_element[1])
+            coins -= points
         else:
             all_events_handled = False
             print(f"Closed! Cannot afford {ingredient}.")
             break
+
 if all_events_handled:
     print("Day completed!")
     print(f"Coins: {coins}")
