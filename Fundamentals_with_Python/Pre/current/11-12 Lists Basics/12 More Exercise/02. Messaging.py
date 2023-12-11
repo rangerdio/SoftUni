c0c71cebcfa2c3f55e0aa3260c133c894a4b37fb
rangerdio <148572548+rangerdio@users.noreply.github.com>
2023-12-11 12:43:49 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/11-12 Lists Basics/12 More Exercise/02. Messaging.py
+++ Fundamentals_with_Python/Pre/current/11-12 Lists Basics/12 More Exercise/02. Messaging.py
@@ -1,18 +1,11 @@
 number_list = input().split()
 message = input()
-# print(number_list)
-# print(message)
-mark = False
 
 for index in range(len(number_list)):
     sum_letters_int = 0
     for letter in number_list[index]:
         sum_letters_int += int(letter)
     number_list[index] = str(sum_letters_int)
-# print()
-# print(number_list)
-# print(message)
-# print(len(message))
 
 for index in range(len(number_list)):
     element = number_list[index]
@@ -20,9 +13,5 @@ for index in range(len(number_list)):
     while element_print > len(message) - 1:
         element_print -= (len(message))
 
-    if mark:
-        print(f"{message[element_print]}", end="")
-        mark = False
-    else:
-        print(f"{message[element_print + index]}", end="")  # Why! without + index Results are terrible
-print()
+    print(f"{message[element_print]}", end="")
+    message = message[:element_print] + message[element_print + 1:]
