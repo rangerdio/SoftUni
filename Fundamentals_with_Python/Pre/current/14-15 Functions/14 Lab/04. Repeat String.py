5931dbd7348081c862f91045905714a9cd3f6209
rangerdio <148572548+rangerdio@users.noreply.github.com>
2023-12-19 07:37:12 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/14-15 Functions/14 Lab/04. Repeat String.py
+++ Fundamentals_with_Python/Pre/current/14-15 Functions/14 Lab/04. Repeat String.py
@@ -1,5 +1,11 @@
+repeat = lambda string, counter: string * counter
+print(repeat(input(), int(input())))
+
+
+"""
 def repeat(string: str, counter: int):
     return string * counter
 
 
 print(repeat(input(), int(input())))
+"""
