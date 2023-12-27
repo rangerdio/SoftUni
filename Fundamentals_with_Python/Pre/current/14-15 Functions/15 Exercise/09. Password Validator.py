7279065aef9156d9b64d125c6c15b38e5deecfbc
rangerdio <148572548+rangerdio@users.noreply.github.com>
2023-12-27 12:44:55 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/14-15 Functions/15 Exercise/09. Password Validator.py
+++ Fundamentals_with_Python/Pre/current/14-15 Functions/15 Exercise/09. Password Validator.py
@@ -1,30 +1,29 @@
 def validator(pass_):
-    is_valid_1 = False
-    is_valid_2 = False
-    is_valid_3 = False
+    is_valid = True
+    digit_length = ""
+
     if 6 > len(pass_) or len(pass_) > 10:
         print("Password must be between 6 and 10 characters")
-    else:
-        is_valid_1 = True
+        is_valid = False
 
     for index, value in enumerate(pass_):
         if not value.isalpha() and not value.isdigit():
             print("Password must consist only of letters and digits")
+            is_valid = False
             break
-        else:
-            is_valid_2 = True
 
     digits = 0
     for index, value in enumerate(pass_):
         if int(value.isdigit()):
             digits += 1
     if digits < 2:
-        print("Password must have at least 2 digits")
-    else:
-        is_valid_3 = True
-    if is_valid_1 and is_valid_2 and is_valid_3:
+        digit_length = "Password must have at least 2 digits"
+        is_valid = False
+    if is_valid:
         return "Password is valid"
+    else:
+        return digit_length
 
 
 password = input()
-pw_check = validator(password)
+print(validator(password))
