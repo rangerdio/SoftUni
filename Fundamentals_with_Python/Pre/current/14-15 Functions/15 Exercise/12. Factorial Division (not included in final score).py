f6a62e750a39e7278d491cc6996f9e8baf6484c8
rangerdio <148572548+rangerdio@users.noreply.github.com>
2023-12-31 09:54:14 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/14-15 Functions/15 Exercise/12. Factorial Division (not included in final score).py
+++ Fundamentals_with_Python/Pre/current/14-15 Functions/15 Exercise/12. Factorial Division (not included in final score).py
@@ -1,5 +1,11 @@
 def factorial_division(number_1, number_2):
-    return
+    factorial_nuber_1 = 1
+    factorial_nuber_2 = 1
+    for num in range(1, number_1 + 1):
+        factorial_nuber_1 *= num
+    for num in range(1, number_2 + 1):
+        factorial_nuber_2 *= num
+    return f"{(factorial_nuber_1 / factorial_nuber_2):.2f}"
 
 
 first_number = int(input())
