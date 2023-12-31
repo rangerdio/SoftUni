d10639553374da33eb5dac65214032f1ce8be622
rangerdio <148572548+rangerdio@users.noreply.github.com>
2023-12-31 09:37:10 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/14-15 Functions/15 Exercise/11. Loading Bar (not included in final score).py
+++ Fundamentals_with_Python/Pre/current/14-15 Functions/15 Exercise/11. Loading Bar (not included in final score).py
@@ -1,7 +1,6 @@
 def loading_bar(number: int):
     complete = (number // 10) * "%"
     incomplete = (10 - number // 10) * "."
-    complete_bar = number + "% [" + complete + incomplete + "]"
     return
 
 
