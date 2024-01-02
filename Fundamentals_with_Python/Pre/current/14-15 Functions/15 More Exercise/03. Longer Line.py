7b2dda6f9a0954bf56bf688c6deec5b22dd7efad
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-01-02 15:16:25 +0200

    PyCharm Update

---
+++ Fundamentals_with_Python/Pre/current/14-15 Functions/15 More Exercise/03. Longer Line.py
@@ -0,0 +1,19 @@
+
+x_1 = 2
+y_1 = 4
+x_2 = -1
+y_2 = 2
+x_3 = -5
+y_3 = -5
+x_4 = 4
+y_4 = -3
+
+# x_1 = float(input())
+# y_1 = float(input())
+# x_2 = float(input())
+# y_2 = float(input())
+# x_3 = float(input())
+# y_3 = float(input())
+# x_4 = float(input())
+# y_4 = float(input())
+#
