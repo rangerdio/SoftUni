2184c6c627f1a6c892ee245353cab659aa1ca685
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-01-02 13:03:48 +0200

    PyCharm Update

---
+++ Fundamentals_with_Python/Pre/current/14-15 Functions/15 More Exercise/02. Center Point.py
@@ -0,0 +1,9 @@
+def center_point(x1: float, y1: float, x2: float, y2: float):
+    return
+
+
+x_1 = float(input())
+y_2 = float(input())
+x_2 = float(input())
+y_1 = float(input())
+print(center_point(x_1, y_1, x_2, y_2))
