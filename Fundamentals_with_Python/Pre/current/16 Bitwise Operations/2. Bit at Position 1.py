bac643f3d409eb8e9c897e7e061eae81c0f05343
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-01-04 17:22:51 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/16 Bitwise Operations/2. Bit at Position 1.py
+++ Fundamentals_with_Python/Pre/current/16 Bitwise Operations/2. Bit at Position 1.py
@@ -1,4 +1,4 @@
-def bit_at_pos(number: int, position: int):
+def bit_at_pos_1(number: int, position: int):
     right_shifted_num = number >> position
     bit = right_shifted_num & 1
     return bit
@@ -6,4 +6,4 @@ def bit_at_pos(number: int, position: int):
 
 num = int(input())
 pos = 1
-print(bit_at_pos(num, pos))
+print(bit_at_pos_1(num, pos))
