2e1d5c0cc32cdda6eecafd8a90cc986cd5d4993f
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-01-06 13:59:17 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/16 Bitwise Operations/5. Odd Times.py
+++ Fundamentals_with_Python/Pre/current/16 Bitwise Operations/5. Odd Times.py
@@ -1,8 +1,8 @@
-
 number_list = list(map(int, input().split()))
-print(number_list)
-
+# print(number_list)
 index = 0
-while index <= len(number_list):
-
+result = 0
+while index < len(number_list):
+    result = result ^ number_list[index]
     index += 1
+print(result)
