359745b02aac31cde62e189305924f011f565c49
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-01-15 13:09:53 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/17 Lab/00. lambda exercises.py
+++ Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/17 Lab/00. lambda exercises.py
@@ -11,3 +11,32 @@
 # print(multiply(3, 2))
 # print(divide(3, 2))
 #
+
+# list1 = [5, 3, 1, 4, 2]
+# sort = sorted(list1, key=lambda x: x)
+# print(sort)
+# sort2 = sorted(list1, key=lambda x: x, reverse=True)
+# print(sort2)
+
+# list2 = [1, 2, 3, 4, 5, 6]
+# filtered_list = list(filter(lambda x: x > 3, list2))
+# print(filtered_list)
+
+# list4 = [1, 2, 3, 4, 5]
+# square = list(map(lambda x: x**2, list4))
+# print(square)
+
+# string = "hello world"
+# cap_words = map(lambda word: word.capitalize(), string.split())
+# cap_string = ' '.join(cap_words)
+# print(cap_string)
+#
+# asd = "ivan".capitalize()
+# print(asd)
+
+
+# check_even_odd = lambda x: "Even" if x % 2 == 0 else "Odd"
+# print(check_even_odd(6))
+# print(check_even_odd(7))
+
+
