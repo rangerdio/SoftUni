64405047657f40327f30127c1e2cef6e77693263
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-01-15 11:29:09 +0200

    PyCharm Update

---
+++ Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/17 Lab/00. lambda exercises.py
@@ -0,0 +1,13 @@
+
+# test = lambda x: x + 10
+# print(test(5))
+#
+# summ = lambda a, b: a + b
+# subtract = lambda a, b: a - b
+# multiply = lambda a, b: a * b
+# divide = lambda a, b: a / b
+# print(summ(3, 2))
+# print(subtract(3, 2))
+# print(multiply(3, 2))
+# print(divide(3, 2))
+#
