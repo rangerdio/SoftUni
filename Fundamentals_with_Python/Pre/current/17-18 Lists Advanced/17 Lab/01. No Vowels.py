e33b71c7c4f4d4495a6bbc2554fd3f5d63dea91d
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-01-18 11:57:56 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/17 Lab/01. No Vowels.py
+++ Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/17 Lab/01. No Vowels.py
@@ -1,4 +1,5 @@
-#text = input().split()
+
+
 text = input()
 vowels = ["a", "o", "u", "e", "i"]
 new_text = ""
@@ -6,7 +7,8 @@ new_text = [new_text + letter for letter in text if letter.lower() not in vowels
 new_text = "".join(new_text)
 print(new_text)
 
-#new_text for letter in text if lower(letter) == "a"
+
+# new_text for letter in text if lower(letter) == "a"
 
 # for letter in text:
 #     if letter.lower() not in vowels:
