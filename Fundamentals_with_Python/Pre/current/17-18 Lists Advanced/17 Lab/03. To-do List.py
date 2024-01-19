1ffdee95fc9cae32fbc7d857885d4979455d3290
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-01-19 21:04:05 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/17 Lab/03. To-do List.py
+++ Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/17 Lab/03. To-do List.py
@@ -4,12 +4,9 @@ while True:
     if todo == "End":
         break
     current_todo = todo.split("-")
-    # print(current_todo)
     todo_list.insert(int(current_todo[0]) - 1, current_todo[1])
     todo_list.pop(int(current_todo[0]))
-    # print(todo_list)
-
-cnt = sum(1 for element in todo_list if element == 0)
-while 0 in todo_list:
-    todo_list.remove(0)
+# while 0 in todo_list:
+#     todo_list.remove(0)
+result = [element for element in todo_list if element != 0]
 print(todo_list)
