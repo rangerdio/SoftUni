1dccf7f18a733289d45064c669368efe363a2c9d
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-01-19 19:08:07 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/17 Lab/02. Trains.py
+++ Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/17 Lab/02. Trains.py
@@ -1,2 +1,9 @@
 wagons = [0] * int(input())
 print(wagons)
+
+while True:
+    command = input()
+    if command == "End":
+        break
+    command_list = [command.split()]
+    print(command_list)
