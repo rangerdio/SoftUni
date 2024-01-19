fcbb88096e9dcf42bf145d95afa3660dd795d732
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-01-19 19:38:17 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/17 Lab/02. Trains.py
+++ Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/17 Lab/02. Trains.py
@@ -1,9 +1,17 @@
-wagons = [0] * int(input())
-print(wagons)
+# train = [0] * int(input())
+wagons = int(input())
+train = []
+train.extend(0 for element in range(wagons))
 
 while True:
     command = input()
     if command == "End":
+        print(train)
         break
-    command_list = [command.split()]
-    print(command_list)
+    command_list = command.split()
+    if command_list[0] == "add":
+        train[-1] += int(command_list[1])
+    elif command_list[0] == "insert":
+        train[int(command_list[1])] += int(command_list[2])
+    elif command_list[0] == "leave":
+        train[int(command_list[1])] -= int(command_list[2])
