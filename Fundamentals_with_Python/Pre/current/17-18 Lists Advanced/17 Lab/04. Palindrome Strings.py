e296930957aa52e1998dbc6785ed787587799fa7
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-01-20 12:44:08 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/17 Lab/04. Palindrome Strings.py
+++ Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/17 Lab/04. Palindrome Strings.py
@@ -3,4 +3,4 @@ palindrome = [word for word in words if word == word[::-1]]
 print(palindrome)
 word = input()
 word_count = palindrome.count(word)
-print(f"Found palindrome {word} times")
+print(f"Found palindrome {word_count} times")
