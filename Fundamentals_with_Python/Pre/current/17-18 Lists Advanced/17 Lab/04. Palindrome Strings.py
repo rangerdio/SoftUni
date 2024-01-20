b2f7e4487a83697445159a93ef2edd85a57b63ae
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-01-20 12:48:29 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/17 Lab/04. Palindrome Strings.py
+++ Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/17 Lab/04. Palindrome Strings.py
@@ -1,6 +1,13 @@
-words = input().split()
-palindrome = [word for word in words if word == word[::-1]]
+palindrome = [word for word in input().split() if word == word[::-1]]
+word_count = palindrome.count(input())
 print(palindrome)
-word = input()
-word_count = palindrome.count(word)
 print(f"Found palindrome {word_count} times")
+
+#
+#
+# words = input().split()
+# palindrome = [word for word in words if word == word[::-1]]
+# print(palindrome)
+# word = input()
+# word_count = palindrome.count(word)
+# print(f"Found palindrome {word_count} times")
