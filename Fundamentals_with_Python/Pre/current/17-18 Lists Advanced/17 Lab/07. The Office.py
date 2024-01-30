aa67b6dbae46fb0ae8dc7fbe77e6d8e5b0012f55
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-01-30 17:16:52 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/17 Lab/07. The Office.py
+++ Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/17 Lab/07. The Office.py
@@ -12,7 +12,8 @@
 # happy = sum(1 for element in happiness_list_factored if element >= average)
 # not_happy = sum(1 for element in happiness_list_factored if element < average)
 #
-# print(f"Score: {happy}/{happy + not_happy}. Employees are happy!") if happy >= not_happy else print(f"Score: {happy}/{happy + not_happy}. Employees are not happy!")
+# print(f"Score: {happy}/{happy + not_happy}. Employees are happy!") \
+#    if happy >= not_happy else print(f"Score: {happy}/{happy + not_happy}. Employees are not happy!")
 
 
 happyness_list = input().split(" ")
@@ -22,4 +23,6 @@ happyness_factored = list(map(lambda x: int(x) * happyness_factor, happyness_lis
 
 above_average = list(filter(lambda x: x >= sum(happyness_factored) / len(happyness_factored), happyness_factored))
 
-print(above_average)
+print(f"Score: {len(above_average)} / {len(happyness_factored)}. Employees are happy!") \
+    if len(above_average) >= len(above_average) else \
+    print(f"Score: {len(above_average)} / {len(happyness_factored)}. Employees are not happy!")
