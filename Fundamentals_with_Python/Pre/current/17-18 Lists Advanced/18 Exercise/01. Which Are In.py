a81723d127914f94d3dcfa4fd7192635503612cb
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-11 09:31:57 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/18 Exercise/01. Which Are In.py
+++ Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/18 Exercise/01. Which Are In.py
@@ -3,8 +3,6 @@ string_list_2 = input().split(", ")
 result = []
 for element in string_list_1:
     for element2 in string_list_2:
-        # result.append(element) if element in element2
-        # break
         if element in element2:
             result.append(element)
             break
