f92cb6d11336aa285cbc76de36c42c20a2f56b6b
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-11 18:40:01 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/18 Exercise/09. Anonymous Threat (not included in final score).py
+++ Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/18 Exercise/09. Anonymous Threat (not included in final score).py
@@ -1,7 +1,14 @@
 def merge_(words_, command):
+    range_upper = int(command[2])
+
+    if int(command[1]) >= len(words_) - 1:
+        return words_
+    elif int(command[2]) >= len(words_) + 2 > int(command[1]):
+        range_upper = len(words_)
+
     new_words_string = ""
     for index in range(len(words_)):
-        if int(command[1]) <= index <= int(command[2]):
+        if int(command[1]) <= index <= range_upper:
             new_words_string += words_[index]
         else:
             word_mod = " " + words_[index] + " "
@@ -20,7 +27,9 @@ while True:
     current_cmd_list = current_cmd.split()
     if current_cmd == "3:1":
         break
-    elif current_cmd[0] == "merge":
+    elif current_cmd_list[0] == "merge":
         words = merge_(words, current_cmd_list)
-    elif current_cmd[0] == "divide":
+    elif current_cmd_list[0] == "divide":
         words = merge_(words, current_cmd_list)
+
+print("".join(words))
--- Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/18 Exercise/10. Pokemon Don't Go (not included in final score).py
+++ Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/18 Exercise/10. Pokemon Don't Go (not included in final score).py
@@ -1,13 +1,14 @@
 def merge_(words_, command):
-    range_ = len(words_)
-    if int(command[1]) >= len(words_):
+    range_upper = int(command[2])
+
+    if int(command[1]) >= len(words_) - 1:
         return words_
-    elif int(command[2]) > len(words_) > int(command[1]):
-        range_ = int(command[2])
+    elif int(command[2]) >= len(words_) + 2 > int(command[1]):
+        range_upper = len(words_)
 
     new_words_string = ""
-    for index in range(range_):
-        if int(command[1]) <= index <= int(command[2]):
+    for index in range(len(words_)):
+        if int(command[1]) <= index <= range_upper:
             new_words_string += words_[index]
         else:
             word_mod = " " + words_[index] + " "
@@ -15,10 +16,20 @@ def merge_(words_, command):
     return new_words_string.split()
 
 
-string = "IvoJohnyTonyBony Mony"
+def divide_(words_, command):
+    return "asd"
+
+
+string = "Ivo Johny Tony Bony Mony"
 words = string.split()
-print(words)
-current_cmd_list = ["merge", "3", "4"]
-words = merge_(words, current_cmd_list)
-print(words)
-print(current_cmd_list)
+while True:
+    current_cmd = input()
+    current_cmd_list = current_cmd.split()
+    if current_cmd == "3:1":
+        break
+    elif current_cmd_list[0] == "merge":
+        words = merge_(words, current_cmd_list)
+    elif current_cmd_list[0] == "divide":
+        words = merge_(words, current_cmd_list)
+
+print("".join(words))
