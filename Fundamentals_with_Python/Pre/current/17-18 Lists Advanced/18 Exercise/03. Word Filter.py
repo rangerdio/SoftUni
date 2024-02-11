2471b5c43ee5d28b7076dbf4ccbc5569aac1a787
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-11 10:42:57 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/18 Exercise/03. Word Filter.py
+++ Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/18 Exercise/03. Word Filter.py
@@ -1,3 +1,4 @@
-string = "kiwi orange banana apple"
-words = string.split()
-print(words)
+filtered_words = [word for word in input().split() if len(word) % 2 == 0]
+# print("\n".join(filtered_words))
+[print(word) for word in filtered_words]
+# [print(word) for word in [word for word in input().split() if len(word) % 2 == 0]]
