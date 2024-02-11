81f02485e7c182ee3d99d5084367d8b30645be1a
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-11 22:54:13 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/18 Exercise/09. Anonymous Threat (not included in final score).py
+++ Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/18 Exercise/09. Anonymous Threat (not included in final score).py
@@ -1,10 +1,10 @@
-def merge_(words_, command):
+def merge_(words_: list, command: list):
     start_index = int(command[1])
     end_index = int(command[2])
 
     if start_index >= len(words_) - 1:
         return words_
-    elif int(command[2]) >= len(words_) + 2 > start_index:
+    elif end_index >= len(words_) + 2 > start_index:
         end_index = len(words_)
 
     new_words_string = ""
@@ -14,14 +14,12 @@ def merge_(words_, command):
         else:
             word_mod = "  " + words_[index] + "  "
             new_words_string += word_mod
-    # print(f"test before split: {new_words_string}")
     return new_words_string.split()
 
 
 def dividing_word(word: str, partitions_: int):
     if len(word) % partitions_ == 0:
         new_word_string = ""
-        # print(f"2) inside dividing_word: {word}, magic_lenght: {magic_lenght}")
         cnt = 1
         for index in range(len(word)):
             if cnt <= 2:
@@ -31,14 +29,11 @@ def dividing_word(word: str, partitions_: int):
                 cnt = 1
                 new_word_string += "  " + word[index]
                 cnt += 1
-        # print(f"3) inside dividing_word, just after divide: {new_word_string}")
         return new_word_string.split("  ")
+
     else:  # if len(word) != 0
         magic_lenght = len(word) // partitions_
-        partitions_up_to = partitions_ - 1
         counter = 0
-        counter2 = 1
-        second_part = ""
         first_part = ""
 
         for index in range(len(word) - magic_lenght - 1):
@@ -50,12 +45,8 @@ def dividing_word(word: str, partitions_: int):
                 first_part += "  "
                 first_part += word[index]
                 counter = 1
-                counter2 += 1
-        # print(f"3.3 first part of dividing string: {first_part}")
         second_part = word[-magic_lenght - 1:]
-        # print(f"3.3) second part of dividing word: {second_part}")
         new_word_string = first_part + "  " + second_part
-        # print(new_word_string)
 
     return new_word_string.split("  ")
 
@@ -64,16 +55,11 @@ def divide_(words_: list, command: list):
     divide_index_number = int(command[1])
     partitions = int(command[2])
     divide_word = words_[divide_index_number]
-    # print(f"1) inside divide_ word to be divided: {divide_word}")
     divided_string_list = dividing_word(divide_word, partitions)
-    # print(divided_string_list)
-    # print(f"4) inside divide, after dividing_word, the divided string: {divided_string_list}")
     divide_result = words_[:divide_index_number] + divided_string_list
-    # print(divide_result)
     return divide_result
 
 
-# string = "abcd efgh ijkl mnop qrst uvwx yz"
 words = input().split()
 while True:
     current_cmd = input()
@@ -82,11 +68,8 @@ while True:
         break
     elif current_cmd_list[0] == "merge":
         words = merge_(words, current_cmd_list)
-        # print(words)
-        # print(f' after merge {" ".join(words)}')
+
     elif current_cmd_list[0] == "divide":
         words = divide_(words, current_cmd_list)
-        # print(words)
-        # print(f'after divide: {" ".join(words)}')
 
 print(" ".join(words))
