6728efecc57fdda3e250e94c7f69a0f658aee723
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-11 09:24:23 +0200

    PyCharm Updatere

--- Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/18 Exercise/01. Which Are In.py
+++ Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/18 Exercise/01. Which Are In.py
@@ -1,4 +1,11 @@
 string_list_1 = input().split(", ")
 string_list_2 = input().split(", ")
-result_list = [element for element in string_list_1 if element in [element2 for element2 in string_list_2]]
-print(result_list)
+result = []
+for element in string_list_1:
+    for element2 in string_list_2:
+        # result.append(element) if element in element2
+        # break
+        if element in element2:
+            result.append(element)
+            break
+print(result)
