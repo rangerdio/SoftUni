408a19da55226093f9f5ad1b2fae51f9451b668c
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-11 10:25:29 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/18 Exercise/02. Next Version.py
+++ Fundamentals_with_Python/Pre/current/17-18 Lists Advanced/18 Exercise/02. Next Version.py
@@ -1,9 +1,11 @@
 ver = "3.9.9"
-version = ver.split(".")
-print(version[2])
-#version[int(len(version) - 1)] += 1
+version = list(map(int, ver.split(".")))
+version[len(version) - 1] += 1
 for index in range(-1, -len(version), -1):
     if version[index] == 10:
         version[index] = 0
         version[index - 1] += 1
-#print(version)
+# result_list = [str(element) for element in version]
+# result = ".".join(result_list)
+# print(result)
+print(".".join([str(element) for element in version]))
