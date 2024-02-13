ca6f7372cc363dd44dc6892b55959443ecad217b
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-13 22:38:05 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/20 Exam Preparation 2/03. Inventory.py
+++ Fundamentals_with_Python/Pre/current/20 Exam Preparation 2/03. Inventory.py
@@ -1,2 +1,17 @@
-journal = input().split(", ")
-print(journal)
+def gather(journal: list, command: list):
+
+    return "123"
+
+
+journal_list = input().split(", ")
+result_journal = []
+
+while True:
+    cmd = input()
+    if cmd == "Craft!":
+        break
+    command_list = cmd.split(" - ")
+    result_journal = gather(journal_list, command_list)
+    journal_list = result_journal
+
+print(", ".join(result_journal))
