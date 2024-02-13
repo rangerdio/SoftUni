bd72c014df72b8da385ac5b37c14f19fac3da00f
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-13 19:15:02 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/20 Exam Preparation 2/01. Counter-Strike_Problem Description.py
+++ Fundamentals_with_Python/Pre/current/20 Exam Preparation 2/01. Counter-Strike_Problem Description.py
@@ -2,31 +2,29 @@ energy = int(input())
 target_energy_distance = 0
 win_counter = 0
 lost = False
-while energy > 0:
+while True:
     command = input()
     if command == "End of battle":
-        print(f"Won battles: {win_counter}. Energy left: {energy}")
         break
     else:
         target_energy_distance = int(command)
 
-    if energy > target_energy_distance:
-        win_counter += 1
-        energy -= target_energy_distance
-        if 0 != win_counter % 3 == 0:
-            energy += win_counter
-        # print(f"--- Target distance {target_energy_distance} ; counter: {win_counter};  Current Energy: {energy}")
-    elif energy == target_energy_distance:
-        energy -= target_energy_distance
-        win_counter += 1
-        if 0 != win_counter % 3 == 0:
-            energy += win_counter
-        # print(f"--- Target distance {target_energy_distance} ; counter: {win_counter};  Current Energy: {energy}")
+    if energy < target_energy_distance:
         lost = True
         break
-    elif energy < target_energy_distance:
-        lost = True
-        # print(f"--- LOST  Target distance {target_energy_distance} ; counter: {win_counter};  Current Energy: {energy}")
-        break
+    if energy == target_energy_distance:
+        win_counter += 1
+        energy -= target_energy_distance
+        if win_counter % 3 == 0:
+            energy += win_counter
+
+    elif energy > target_energy_distance:
+        win_counter += 1
+        energy -= target_energy_distance
+        if win_counter % 3 == 0:
+            energy += win_counter
+
 if lost:
     print(f"Not enough energy! Game ends with {win_counter} won battles and {energy} energy")
+else:
+    print(f"Won battles: {win_counter}. Energy left: {energy}")
