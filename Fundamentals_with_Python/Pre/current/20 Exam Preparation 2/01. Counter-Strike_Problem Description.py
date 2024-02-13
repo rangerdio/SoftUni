8615885a00dbe1126710ed72cbb8ef489171a849
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-13 20:36:27 +0200

    PyCharm Update

--- Fundamentals_with_Python/Pre/current/20 Exam Preparation 2/01. Counter-Strike_Problem Description.py
+++ Fundamentals_with_Python/Pre/current/20 Exam Preparation 2/01. Counter-Strike_Problem Description.py
@@ -1,30 +1,23 @@
-energy = int(input())
-target_energy_distance = 0
-win_counter = 0
-lost = False
-while True:
-    command = input()
-    if command == "End of battle":
-        break
-    else:
-        target_energy_distance = int(command)
+def counter_strike(energy: int):
+    target_energy_distance = 0
+    win_counter = 0
 
-    if energy < target_energy_distance:
-        lost = True
-        break
-    if energy == target_energy_distance:
-        win_counter += 1
-        energy -= target_energy_distance
-        if win_counter % 3 == 0:
-            energy += win_counter
+    while True:
+        command = input()
+        if command == "End of battle":
+            print(f"Won battles: {win_counter}. Energy left: {energy}")
+            break
+        else:
+            target_energy_distance = int(command)
 
-    elif energy > target_energy_distance:
-        win_counter += 1
-        energy -= target_energy_distance
-        if win_counter % 3 == 0:
-            energy += win_counter
+        if energy >= target_energy_distance:
+            win_counter += 1
+            energy -= target_energy_distance
+            energy += win_counter if win_counter % 3 == 0 else 0
+        else:
+            print(f"Not enough energy! Game ends with {win_counter} won battles and {energy} energy")
+            break
+    return
 
-if lost:
-    print(f"Not enough energy! Game ends with {win_counter} won battles and {energy} energy")
-else:
-    print(f"Won battles: {win_counter}. Energy left: {energy}")
+
+counter_strike(int(input()))
