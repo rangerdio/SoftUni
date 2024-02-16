d226a1b00889a5c4b865ab74698f5b7b49921172
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-17 00:08:52 +0200

    Pycharm Update

--- Fundamentals_with_Python/Pre/current/Mid Exams by Problem/Mid Problem 1/01. Bonus Scoring System.py
+++ Fundamentals_with_Python/Pre/current/Mid Exams by Problem/Mid Problem 1/01. Bonus Scoring System.py
@@ -1,3 +1,6 @@
+# import math
+#
+#
 # def student_score(number_of_lectures_, additional_bonus_, student_attendances_):
 #     total_bonus = student_attendances_ / number_of_lectures_ * (5 + additional_bonus_)
 #     return total_bonus
@@ -14,10 +17,11 @@
 #
 # max_bonus = max(results)
 # max_bonus_index = results.index(max(results))
-# print(f"Max Bonus: {round(max_bonus)}.")
+# print(f"Max Bonus: {math.ceil(max_bonus)}.")
 # print(f"The student has attended {attendances[max_bonus_index]} lectures.")
 
 import sys
+import math
 max_result = -sys.float_info.max
 max_result_attendances = 0
 number_of_students, number_of_lectures, additional_bonus = int(input()), int(input()), int(input())
@@ -29,5 +33,5 @@ for student in range(number_of_students):
         max_result = total_bonus
         max_result_attendances = student_attendances
 
-print(f"Max Bonus: {round(max_result)}.")
+print(f"Max Bonus: {math.ceil(max_result)}.")
 print(f"The student has attended {max_result_attendances} lectures.")
