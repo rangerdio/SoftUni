deecd936312e4b4ea9513a38490c2ee10ea48998
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-17 19:54:17 +0200

    Pycharm Update

--- Fundamentals_with_Python/Pre/current/Mid Exams by Problem/Mid Problem 2/02. Treasure Hunt.py
+++ Fundamentals_with_Python/Pre/current/Mid Exams by Problem/Mid Problem 2/02. Treasure Hunt.py
@@ -14,14 +14,15 @@ def drop(treasure: list, cmd: list):
 def steal(treasure: list, cmd: list):
     cmd = int(cmd[0])
     if cmd >= len(treasure):
+        stolen_items = treasure
+        print(f'{", ".join(stolen_items)}')
         treasure = []
     else:
         stolen_items = []
         for _ in range(cmd):
             stolen_items.append(treasure.pop(-1))
-        print(stolen_items[::-1])
+        print(f'{", ".join(stolen_items[::-1])}')
     return treasure
-
 
 
 treasure_chest = input().split("|")
@@ -47,4 +48,8 @@ while True:
         resulted_treasure = steal(treasure_chest, command_list)
         treasure_chest = resulted_treasure
 
-print(treasure_chest)
+if not treasure_chest:
+    print("Failed treasure hunt.")
+else:
+    result_lenght = len("".join(treasure_chest))
+    print(f"Average treasure gain: {(result_lenght / len(treasure_chest)):.2f} pirate credits.")
