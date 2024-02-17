b8195e1a5158d23b1deb3b607beec1f71edbe78b
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-17 21:21:27 +0200

    Pycharm Update

--- Fundamentals_with_Python/Pre/current/Mid Exams by Problem/Mid Problem 3/03. Man O War.py
+++ Fundamentals_with_Python/Pre/current/Mid Exams by Problem/Mid Problem 3/03. Man O War.py
@@ -1,6 +1,45 @@
+def firing(pirate_ship: list, warship: list, index: int, damage: int):
+    if len(warship) > index:
+        warship[index] -= damage
+        if warship[index] <= 0:
+            print("You won! The enemy ship has sunken.")
+            # ENEMY SHIP IS DOWN AND PROGRAM END
+            return
+    return [pirate_ship, warship]
+
+
+def defending(pirate_ship: list, warship: list, start_index: int, end_index: int, damage: int):
+    valid_list = []
+    for _ in range(len(pirate_ship)):
+        valid_list.append(_)
+    if start_index in valid_list and end_index in valid_list:
+        for index in range(start_index, end_index + 1):
+            pirate_ship[index] -= damage
+            if pirate_ship[index] <= 0:
+                print("You lost! The pirate ship has sunken.")
+                # WE LOST. OUR SHIP IS DOWN AND PROGRAM END
+                return
+    return [pirate_ship, warship]
+
+
+def repairing(pirate_ship: list, warship: list, index: int, health: int, max_health: int):
+    valid_list = []
+    for _ in range(len(pirate_ship)):
+        valid_list.append(_)
+    if index in valid_list:
+        pirate_ship[index] += health
+        if pirate_ship[index] > max_health:
+            pirate_ship[index] = max_health
+    return [pirate_ship, warship]
+
+
+def status(pirate_ship: list, warship: list, max_health: int):
+    return
+
+
 pirate_ship_status = input().split(">")
-warship_status = input()
-max_health = input()
+warship_status = input().split(">")
+max_section_health = int(input())
 
 while True:
     command = input()
@@ -8,3 +47,13 @@ while True:
     if command == "Retire":
         break
 
+    command_list = command.split()
+
+    if command == "Fire":
+        firing(pirate_ship_status, warship_status, int(command_list[1]), int(command_list[2]))
+    elif command == "Defend":
+        defending(pirate_ship_status, warship_status, int(command_list[1]), int(command_list[2]), int(command_list[3]))
+    elif command == "Repair":
+        repairing(pirate_ship_status, warship_status, int(command_list[1]), int(command_list[2]), max_section_health)
+    elif command == "Status":
+        status(pirate_ship_status, warship_status, max_section_health)
