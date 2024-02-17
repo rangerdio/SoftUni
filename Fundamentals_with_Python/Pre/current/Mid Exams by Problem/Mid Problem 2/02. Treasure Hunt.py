4a11acbd69829e699e7096e59dc6a4e1404753fc
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-17 19:24:27 +0200

    Pycharm Update

--- Fundamentals_with_Python/Pre/current/Mid Exams by Problem/Mid Problem 2/02. Treasure Hunt.py
+++ Fundamentals_with_Python/Pre/current/Mid Exams by Problem/Mid Problem 2/02. Treasure Hunt.py
@@ -1,13 +1,27 @@
-def loot():
-    return "asd"
+def loot(treasure: list, cmd: list):
+    for item in cmd:
+        if item not in treasure:
+            treasure.insert(0, item)
+    return treasure
 
 
-def drop():
-    return "123"
+def drop(treasure: list, cmd: list):
+    cmd = int(cmd[0])
+    treasure.append(treasure.pop(cmd))
+    return treasure
 
 
-def steal():
-    return "123"
+def steal(treasure: list, cmd: list):
+    cmd = int(cmd[0])
+    if cmd >= len(treasure):
+        treasure = []
+    else:
+        stolen_items = []
+        for _ in range(cmd):
+            stolen_items.append(treasure.pop(-1))
+        print(stolen_items[::-1])
+    return treasure
+
 
 
 treasure_chest = input().split("|")
@@ -19,8 +33,18 @@ while True:
     command_list = command.split()
 
     if command_list[0] == "Loot":
-        pass
+        command_list.remove("Loot")
+        resulted_treasure = loot(treasure_chest, command_list)
+        treasure_chest = resulted_treasure
+
     elif command_list[0] == "Drop":
-        pass
-    elif command_list[0] == "steal":
-        pass
+        command_list.remove("Drop")
+        resulted_treasure = drop(treasure_chest, command_list)
+        treasure_chest = resulted_treasure
+
+    elif command_list[0] == "Steal":
+        command_list.remove("Steal")
+        resulted_treasure = steal(treasure_chest, command_list)
+        treasure_chest = resulted_treasure
+
+print(treasure_chest)
