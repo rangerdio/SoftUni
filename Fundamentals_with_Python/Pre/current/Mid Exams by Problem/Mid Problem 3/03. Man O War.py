bba13d013aee73e99a868b88e5d3f13ae7b336f5
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-17 22:31:59 +0200

    Pycharm Update

--- Fundamentals_with_Python/Pre/current/Mid Exams by Problem/Mid Problem 3/03. Man O War.py
+++ Fundamentals_with_Python/Pre/current/Mid Exams by Problem/Mid Problem 3/03. Man O War.py
@@ -1,14 +1,15 @@
-def firing(pirate_ship: list, warship: list, index: int, damage: int):
+def firing(pirate_ship: list, warship: list, index: int, damage: int, kk: str):
     if len(warship) > index:
         warship[index] -= damage
         if warship[index] <= 0:
             print("You won! The enemy ship has sunken.")
             # ENEMY SHIP IS DOWN AND PROGRAM END
-            return
-    return [pirate_ship, warship]
+            kk = "bbb"
+            return [kk, pirate_ship, warship]
+    return [kk, pirate_ship, warship]
 
 
-def defending(pirate_ship: list, warship: list, start_index: int, end_index: int, damage: int):
+def defending(pirate_ship: list, warship: list, start_index: int, end_index: int, damage: int, kk: str):
     valid_list = []
     for _ in range(len(pirate_ship)):
         valid_list.append(_)
@@ -18,11 +19,12 @@ def defending(pirate_ship: list, warship: list, start_index: int, end_index: int
             if pirate_ship[index] <= 0:
                 print("You lost! The pirate ship has sunken.")
                 # WE LOST. OUR SHIP IS DOWN AND PROGRAM END
-                return
-    return [pirate_ship, warship]
+                kk = "bbb"
+                return [kk, pirate_ship, warship]
+    return [kk, pirate_ship, warship]
 
 
-def repairing(pirate_ship: list, warship: list, index: int, health: int, max_health: int):
+def repairing(pirate_ship: list, warship: list, index: int, health: int, max_health: int, kk: str):
     valid_list = []
     for _ in range(len(pirate_ship)):
         valid_list.append(_)
@@ -30,33 +32,49 @@ def repairing(pirate_ship: list, warship: list, index: int, health: int, max_hea
         pirate_ship[index] += health
         if pirate_ship[index] > max_health:
             pirate_ship[index] = max_health
-    return [pirate_ship, warship]
+    return [kk, pirate_ship, warship]
 
 
-def status(pirate_ship: list, warship: list, max_health: int):
+def status(pirate_ship: list, max_health: int):
     barer = 20 / 100 * max_health
 
     unhealthy = [element for element in pirate_ship if element < barer]
-    return len(unhealthy)
+    return f"{len(unhealthy)} sections need repair."
 
 
-pirate_ship_status = input().split(">")
-warship_status = input().split(">")
+pirate_ship_status = list(map(int, input().split(">")))
+warship_status = list(map(int, input().split(">")))
 max_section_health = int(input())
 
 while True:
     command = input()
-
+    kkk = "aaa"
     if command == "Retire":
+        print(f"Pirate ship status: {sum(pirate_ship_status)}")
+        print(f"Warship status: {sum(warship_status)}")
         break
 
     command_list = command.split()
+    if command_list[0] == "Fire":
+        result = firing(pirate_ship_status, warship_status, int(command_list[1]), int(command_list[2]), kkk)
+        if result[0] == "bbb":
+            break
+        else:
+            pirate_ship_status = result[1]
+            warship_status = result[2]
 
-    if command == "Fire":
-        firing(pirate_ship_status, warship_status, int(command_list[1]), int(command_list[2]))
-    elif command == "Defend":
-        defending(pirate_ship_status, warship_status, int(command_list[1]), int(command_list[2]), int(command_list[3]))
-    elif command == "Repair":
-        repairing(pirate_ship_status, warship_status, int(command_list[1]), int(command_list[2]), max_section_health)
-    elif command == "Status":
-        status(pirate_ship_status, warship_status, max_section_health)
+    elif command_list[0] == "Defend":
+        result = defending(pirate_ship_status, warship_status, int(command_list[1]), int(command_list[2]), int(command_list[3]), kkk)
+        if result[0] == "bbb":
+            break
+        else:
+            pirate_ship_status = result[1]
+            warship_status = result[2]
+
+    elif command_list[0] == "Repair":
+        result = repairing(pirate_ship_status, warship_status, int(command_list[1]), int(command_list[2]), max_section_health, kkk)
+        pirate_ship_status = result[1]
+        warship_status = result[2]
+
+    elif command_list[0] == "Status":
+        print(status(pirate_ship_status, max_section_health))
