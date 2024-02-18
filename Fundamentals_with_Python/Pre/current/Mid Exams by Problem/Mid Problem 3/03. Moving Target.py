dbe683da7ce777f69ef6f038c1851a3db8add14c
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-18 07:54:27 +0200

    Pycharm Update

--- Fundamentals_with_Python/Pre/current/Mid Exams by Problem/Mid Problem 3/03. Moving Target.py
+++ Fundamentals_with_Python/Pre/current/Mid Exams by Problem/Mid Problem 3/03. Moving Target.py
@@ -1,15 +1,30 @@
-def manipulation(targets):
+def index_validation(data_list: list, index: int):
+    valid_list = []
+    for _ in range(len(data_list)):
+        valid_list.append(_)
+    if index in valid_list:
+        return True
+    else:
+        return False
+
+
+def manipulation(targets: list, index: int, value: int):
+
     return "123"
 
 
 target_list = input().split()
+result_list = []
 while True:
     command = input()
     if command == "End":
         print(f'{" ".join(target_list)}')
         break
     command_list = command.split()
-    result_list = manipulation(target_list)
+    if command_list[0] == "Shoot":
+        result_list = manipulation(target_list, int(command_list[1]), int(command_list[2]))
+    elif command_list[0] == "Add":
+        result_list = manipulation(target_list, int(command_list[1]), int(command_list[2]))
+    elif command_list[0] == "Add":
+        result_list = manipulation(target_list, int(command_list[1]), int(command_list[2]))
     target_list = result_list
-
-
