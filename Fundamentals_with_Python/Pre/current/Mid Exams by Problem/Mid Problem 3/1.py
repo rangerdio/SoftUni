4af0e23f8b9195d336510e445ca92c10c8973c9a
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-18 10:34:17 +0200

    Pycharm Update

--- Fundamentals_with_Python/Pre/current/Mid Exams by Problem/Mid Problem 3/1.py
+++ Fundamentals_with_Python/Pre/current/Mid Exams by Problem/Mid Problem 3/1.py
@@ -1,16 +1,24 @@
-def index_validation(data_list: list, index: int):
-    valid_list = []
-    for _ in range(len(data_list)):
-        valid_list.append(_)
-    if index in valid_list:
-        return True
-    else:
-        return False
+# # index = 1
+# # asd = [1, 2, 3, 4, 5, 6]
+# #
+# # print(asd[:5])
+#
+# asd = [2]
+# def find_average_number(numbers: list):
+#     if not numbers:
+#         return "not_exist"
+#     avr_number_index = len(numbers) // 2
+#     return [numbers[avr_number_index], avr_number_index]
+#
+#
+# print(find_average_number(asd)[0])
 
 
-asd = [1, 2, 3, 4, 5]
+def get_elements_above_average(numbers: list, avr: float):
+    above_avr = [element for element in numbers if element > avr]
+    print(above_avr)
+    return above_avr
 
-if index_validation(asd, 4):
-    print(f"asd")
-else:
-    print(f"kuku")
+
+asd = [-1, -2, -3, -4, -5, -6]
+print(get_elements_above_average(asd, -3.5))
