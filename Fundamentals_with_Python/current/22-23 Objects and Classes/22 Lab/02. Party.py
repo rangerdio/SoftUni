b7fa46be20b03615afb40004a2127a97b3423ee2
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-20 22:08:29 +0200

    Pycharm Update

---
+++ Fundamentals_with_Python/current/22-23 Objects and Classes/22 Lab/02. Party.py
@@ -0,0 +1,15 @@
+class Party:
+    counter = 0
+    def __init__(self):
+        self.people = []
+
+
+party = Party()
+name = input()
+while name != "End":
+    party.people.append(name)
+    Party.counter += 1
+    name = input()
+
+print(f"Going: {', '.join(party.people)}")
+print(f"Total: {total_people_going}")
