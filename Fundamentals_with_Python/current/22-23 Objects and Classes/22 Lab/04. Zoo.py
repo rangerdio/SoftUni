5a13a1003af6c8e064771e0c69e848b1877d7a56
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-22 21:33:00 +0200

    Pycharm Update

--- Fundamentals_with_Python/current/22-23 Objects and Classes/22 Lab/04. Zoo.py
+++ Fundamentals_with_Python/current/22-23 Objects and Classes/22 Lab/04. Zoo.py
@@ -1,6 +1,35 @@
 class Zoo:
     __animals = 1
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, zoo_name):
+        self.zoo_name = zoo_name
+        self.mammals = []
+        self.fish = []
+        self.birds = []
 
+    def add_animal(self, species, name):
+        if species == "mammal":
+            self.mammals.append(name)
+        elif species == "fish":
+            self.fish.append(name)
+        elif species == "bird":
+            self.birds.append(name)
+        Zoo.__animals += 1
+
+    def get_info(self, species):
+        result = ""
+        if species == "mammal":
+            pass
+        elif species == "fish"
+            pass
+        elif species == "bird":
+            pass
+
+
+        return f"{self.species} in {self.zoo_name}: {self.name}\nTotal animals: {Zoo.__animals}"
+
+
+name_ = input()
+total = int(input())
+for i in range(total):
+    animal_info = input().split()
