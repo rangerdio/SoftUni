1372c4d1ac2c550c98f4c27f724869d60dbbc5dc
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-22 22:19:07 +0200

    Pycharm Update

--- Fundamentals_with_Python/current/22-23 Objects and Classes/22 Lab/05. Circle.py
+++ Fundamentals_with_Python/current/22-23 Objects and Classes/22 Lab/05. Circle.py
@@ -5,12 +5,19 @@ class Circle:
         self.diameter = diameter
         self.radius = diameter / 2
 
-    def calculate_circumference(self, radius):
-        return 2 * Circle.__pi * radius
+    def calculate_circumference(self):
+        return 2 * Circle.__pi * self.radius
 
-    def calculate_area(self, radius):
-        return Circle.__pi ** radius
+    def calculate_area(self):
+        return Circle.__pi * self.radius ** 2
 
     def calculate_area_of_sector(self, angle):
-        pass
+        return (angle / 360) * self.radius ** 2 * Circle.__pi
 
+
+circle = Circle(10)
+angle = 5
+
+print(f"{circle.calculate_circumference():.2f}")
+print(f"{circle.calculate_area():.2f}")
+print(f"{circle.calculate_area_of_sector(angle):.2f}")
