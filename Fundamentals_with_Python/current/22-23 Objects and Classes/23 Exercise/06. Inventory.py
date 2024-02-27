696b8e12928740b2aafef74d07ec2a8cfd62b9d3
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-27 22:24:04 +0200

    Pycharm Update

---
+++ Fundamentals_with_Python/current/22-23 Objects and Classes/23 Exercise/06. Inventory.py
@@ -0,0 +1,14 @@
+class Inventory:
+
+    def __init__(self, __capacity: int):
+        self.__capacity = __capacity
+        items = []
+
+    def add_item(self, item: str):
+        pass
+
+    def get_capacity(self):
+        pass
+
+    def __repr__(self):
+        pass
