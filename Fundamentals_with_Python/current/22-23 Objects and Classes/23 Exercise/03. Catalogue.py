151737ff2a9c38215321629f6f434d557530854d
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-02-27 20:53:26 +0200

    Pycharm Update

--- Fundamentals_with_Python/current/22-23 Objects and Classes/23 Exercise/03. Catalogue.py
+++ Fundamentals_with_Python/current/22-23 Objects and Classes/23 Exercise/03. Catalogue.py
@@ -11,9 +11,10 @@ class Catalogue:
     def get_by_letter(self, first_letter: str):
         return [element for element in Catalogue.products if element[0] == first_letter]
 
-
     def __repr__(self):
-        pass
+        new_list = sorted(Catalogue.products)
+        new_list.insert(0, f"Items in the {self.name} catalogue:")
+        return "\n".join(new_list)
 
 
 catalogue = Catalogue("Furniture")
