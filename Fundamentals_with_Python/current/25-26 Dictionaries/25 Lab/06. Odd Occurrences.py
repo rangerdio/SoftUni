b31a9c6a3e81b116db17c63e3441cdcd684a2a6e
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-02 11:41:11 +0200

    Pycharm Update

---
+++ Fundamentals_with_Python/current/25-26 Dictionaries/25 Lab/06. Odd Occurrences.py
@@ -0,0 +1,16 @@
+asd = "a a A SQL xx a xx a A a XX c"
+element_list = asd.split()
+# element_list = input().split()
+elements = {}
+elements_list_lowered = []
+[elements_list_lowered.append(word.lower()) for word in element_list]
+
+for word in elements_list_lowered:
+    if word not in elements.keys():
+        elements[word] = 0
+    else:
+        elements[word] += 1
+
+for word, value in elements.items():
+    if value % 2 == 0:
+        print(word, end=" ")
