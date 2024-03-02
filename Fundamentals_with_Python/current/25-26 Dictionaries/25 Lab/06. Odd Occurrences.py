762e47362c4c543f95f76e17dde2b5caa7f425bf
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-02 11:48:36 +0200

    Pycharm Update

--- Fundamentals_with_Python/current/25-26 Dictionaries/25 Lab/06. Odd Occurrences.py
+++ Fundamentals_with_Python/current/25-26 Dictionaries/25 Lab/06. Odd Occurrences.py
@@ -1,13 +1,12 @@
 element_list = input().split()
 elements = {}
-elements_list_lowered = []
-[elements_list_lowered.append(word.lower()) for word in element_list]
+# elements_list_lowered = []
+# [elements_list_lowered.append(word.lower()) for word in element_list]
 
-for word in elements_list_lowered:
-    if word not in elements.keys():
-        elements[word] = 0
-    else:
-        elements[word] += 1
+for word in element_list:
+    if word.lower() not in elements.keys():
+        elements[word.lower()] = 0
+    elements[word.lower()] += 1
 
 for word, value in elements.items():
     if value % 2 == 0:
