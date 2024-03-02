9ab6be1863344162bd5d2bcbcbd899e0fcc53e42
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-02 14:49:59 +0200

    Pycharm Update

--- Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/02. A Miner Task.py
+++ Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/02. A Miner Task.py
@@ -1,7 +1,14 @@
 def collect(data_to_collect: list):
     results = {}
     for index in range(0, len(data_to_collect), 2):
-        results[data_to_collect[index]] = int(data_to_collect[index + 1])
+        key = data_to_collect[index]
+        quantity = data_to_collect[index + 1]
+
+        if key not in results.keys():
+            results[key] = int(quantity)
+        else:
+            results[key] += int(quantity)
+
     return [print(f"{resource_} -> {quantity_}") for resource_, quantity_ in results.items()]
 
 
