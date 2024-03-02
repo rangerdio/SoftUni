012604c264426ea37ceebd63d6be7f57ccf040f1
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-02 13:45:02 +0200

    Pycharm Update

--- Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/01. Count Chars in a String.py
+++ Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/01. Count Chars in a String.py
@@ -2,14 +2,13 @@ text_list = input().split()
 letters = {}
 
 for word in text_list:
-    word = word.lower()
+    # word = word.lower()
 
     for letter in word:
         if letter not in letters:
             letters[letter] = 1
         else:
             letters[letter] += 1
-
 
 for letter, hits in letters.items():
     print(f"{letter} -> {hits}")
