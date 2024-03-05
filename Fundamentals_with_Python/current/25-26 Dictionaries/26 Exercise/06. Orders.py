5d391c1dd726da809c20f2b51f36a12d5b45014a
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-05 20:06:26 +0200

    Pycharm Update

--- Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/06. Orders.py
+++ Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/06. Orders.py
@@ -5,15 +5,19 @@ while True:
         break
     current_item, current_price, current_quantity = command[0], command[1], command[2]
 
-    if current_item not in items:
-        items[current_item] = {float(current_price): int(current_quantity)}
+    if current_item not in items.keys():
+        items[current_item] = {"price": float(current_price), "quantity": int(current_quantity)}
 
     else:   # current_item exists
-        for key, value in items[current_item].items():
-            items[current_item] = current_price
-            items[current_item][key] += int(current_quantity)
+        if items[current_item]["price"] != float(current_price):
+            items[current_item]["price"] = float(current_price)
+        items[current_item]["quantity"] += int(current_quantity)
+
+
 print(items)
 
+for item in items:
+    print(item)
 
 # items = {}
 # while True:
---
+++ Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/07. SoftUni Parking.py
@@ -0,0 +1,2 @@
+asd = {"ivan": 1, "koko": 2, "boko": 3}
+print(asd["ivan"])
