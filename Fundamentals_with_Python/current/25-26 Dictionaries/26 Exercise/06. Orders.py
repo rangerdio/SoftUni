81795027c43547739b2220605878df1475d1a7a2
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-05 20:27:58 +0200

    Pycharm Update

--- Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/06. Orders.py
+++ Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/06. Orders.py
@@ -13,29 +13,8 @@ while True:
             items[current_item]["price"] = float(current_price)
         items[current_item]["quantity"] += int(current_quantity)
 
+for item in items.keys():
+    items[item] = items[item]["price"] * items[item]["quantity"]
 
-print(items)
-
-for item in items:
-    print(item)
-
-# items = {}
-# while True:
-#     command = input().split()
-#     if command[0] == "buy":
-#         break
-#     current_item, current_price, current_quantity = command[0], command[1], command[2]
-#
-#     if current_item not in items:
-#         items[current_item] = {float(current_price): int(current_quantity)}
-#
-#     else:   # current_item exists
-#         for key, value in items[current_item].items():
-#             items[current_item] = current_price
-#             items[current_item][key] += int(current_quantity)
-#
-# print(items)
-#
-# # for element in current_items:
-# #     for key in element.keys():
-# #         print(f"{key} -> {element[key]:.2f}")
+for item in items.keys():
+    print(f"{item} -> {items[item]:.2f}")
