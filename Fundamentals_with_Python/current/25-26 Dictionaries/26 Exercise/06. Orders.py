2ce5eaa9cc3e4cf983c3d15975dfada1f7f9b45d
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-05 19:27:37 +0200

    Pycharm Update

--- Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/06. Orders.py
+++ Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/06. Orders.py
@@ -7,13 +7,31 @@ while True:
 
     if current_item not in items:
         items[current_item] = {float(current_price): int(current_quantity)}
-    
+
     else:   # current_item exists
         for key, value in items[current_item].items():
+            items[current_item] = current_price
             items[current_item][key] += int(current_quantity)
-
 print(items)
 
-# for element in current_items:
-#     for key in element.keys():
-#         print(f"{key} -> {element[key]:.2f}")
+
+# items = {}
+# while True:
+#     command = input().split()
+#     if command[0] == "buy":
+#         break
+#     current_item, current_price, current_quantity = command[0], command[1], command[2]
+#
+#     if current_item not in items:
+#         items[current_item] = {float(current_price): int(current_quantity)}
+#
+#     else:   # current_item exists
+#         for key, value in items[current_item].items():
+#             items[current_item] = current_price
+#             items[current_item][key] += int(current_quantity)
+#
+# print(items)
+#
+# # for element in current_items:
+# #     for key in element.keys():
+# #         print(f"{key} -> {element[key]:.2f}")
