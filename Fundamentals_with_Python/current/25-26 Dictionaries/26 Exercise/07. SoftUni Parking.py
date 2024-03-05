46bf22973a75ba70d1b438721c853efc5ce505dd
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-05 20:46:48 +0200

    Pycharm Update

--- Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/07. SoftUni Parking.py
+++ Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/07. SoftUni Parking.py
@@ -1,1 +1,16 @@
+def register():
+    return
+
+
+def unregister():
+    return
+
+
 n = int(input())
+parking = {}
+for _ in range(n):
+    command_list = input().split()
+    if command_list[0] == "register":
+        pass
+    elif command_list[0] == "unregister":
+        pass
