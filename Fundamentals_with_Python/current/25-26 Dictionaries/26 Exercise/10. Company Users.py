c77e6b90ef98e59acbb483356793b41933ceb7b3
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-07 11:02:24 +0200

    Pycharm Update

--- Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/10. Company Users.py
+++ Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/10. Company Users.py
@@ -7,10 +7,26 @@ while True:
     if company not in companies.keys():
         companies[company] = [emp_id]
     else:   # company exists, so there is at least one employee in its list
-        companies[company].append(emp_id)
+        if emp_id not in companies[company]:
+            companies[company].append(emp_id)
 # print(companies)
 
 for company in companies.keys():
     print(company)
     for empID in companies[company]:
         print(f"-- {empID}")
+#
+#
+# companies = {}
+# while True:
+#     data = input().split(" -> ")
+#     if data[0] == "End":
+#         break
+#     company, emp_id = data[0], data[1]
+#
+# # print(companies)
+#
+# for company in companies.keys():
+#     print(company)
+#     for empID in companies[company]:
+#         print(f"-- {empID}")
