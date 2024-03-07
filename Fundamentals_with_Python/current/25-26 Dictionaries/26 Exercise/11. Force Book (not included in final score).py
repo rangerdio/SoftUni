a9caf36e034388d057a1a84b081925c516780054
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-07 23:51:07 +0200

    Pycharm Update

--- Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/11. Force Book (not included in final score).py
+++ Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/11. Force Book (not included in final score).py
@@ -27,9 +27,39 @@ while True:
     elif " -> " in data:
         current_user, current_side = data.split(" -> ")[0], data.split(" -> ")[1]
 
+        if current_side not in force.keys():    # no such force side and # no such force user (3rd option)
+            notExist = True
+            for side in force.keys():
+                if current_user in force[side]:
+                    notExist = False
+            if notExist:
+                force[current_side] = [current_user]
+                print(f"{current_user} joins the {current_side} side!")
 
+        elif current_side in force.keys():  # there is such force side existing
+            notExist = True
+            for side in force.keys():
+                if current_user in force[side]:
+                    notExist = False
 
-    print(f"Current force:  {force}")
+            if notExist:  # but not such force user in any side
+                force[current_side].append(current_user)
+                print(f"{current_user} joins the {current_side} side!")
+            else:   # but user exists
+                # print("we come here???")
+                side_to_remove = ""
+                side_to_add = ""
+                for side in force.keys():
+                    if current_user not in force[side]:
+                        side_to_add = side
+                for side in force.keys():
+                    if current_user in force[side]:
+                        side_to_remove = side
+                force[side_to_remove].remove(current_user)
+                force[side_to_add].append(current_user)
+                print(f"{current_user} joins the {side_to_add} side!")
+
+    #  print(f"Current force:  {force}")
 
 for side in force.keys():
     if force[side]:
