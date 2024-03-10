af6e295285ef38fe9b260b70622ea630f259d6a3
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-10 11:16:37 +0200

    Pycharm Update

--- Fundamentals_with_Python/current/28-29 Text Processing/29 Exercise/02. Character Multiplier.py
+++ Fundamentals_with_Python/current/28-29 Text Processing/29 Exercise/02. Character Multiplier.py
@@ -1,1 +1,34 @@
-print("@".isascii())
+def get_extension(words: list):  # get the additional value if one of the strings is longer than other
+    len_ = 0
+    adds_string = ""
+    adds_number = 0
+    if len(words[0]) < len(words[1]):
+        len_ = len(words[0])
+        adds_string = words[1][len_::]
+    else:
+        len_ = len(words[1])
+        adds_string = words[0][len_::]
+
+    for letter in adds_string:
+        adds_number += ord(letter)
+
+    return adds_number
+
+
+def get_calculation(strings_: list):    # calculate the value based on the shorter one
+    total_number = 0
+    string_1, string_2 = strings_[0], strings_[1]
+    for i in range(len(string_1)):
+        total_number += ord(string_1[i]) * ord(string_2[i])
+
+    return total_number
+
+
+strings = input().split()
+
+if len(strings[0]) > len(strings[1]):   # make sure that first string is the shorter if different lenght
+    strings[0], strings[1] = strings[1], strings[0]
+
+total = get_calculation(strings) + get_extension(strings)
+print(total)
+
