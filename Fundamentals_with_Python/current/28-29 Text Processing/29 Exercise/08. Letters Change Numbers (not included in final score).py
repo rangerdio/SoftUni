734c282e8223f09a83c6bef0b714721dd306e836
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-11 23:54:43 +0200

    k

--- Fundamentals_with_Python/current/28-29 Text Processing/29 Exercise/08. Letters Change Numbers (not included in final score).py
+++ Fundamentals_with_Python/current/28-29 Text Processing/29 Exercise/08. Letters Change Numbers (not included in final score).py
@@ -1,31 +1,63 @@
 def uppercase_front(word: str):
-    pass
+    letter = word[0].lower()
+    letter_position = ord(letter) - 96
+    number = ""
+    for letter_ in word[1:]:
+        if letter_.isnumeric():
+            number += letter_
+    number = int(number)
+    return number / letter_position
 
 
 def lowercase_front(word: str):
-    pass
+    letter = word[0].lower()
+    letter_position = ord(letter) - 96
+    number = ""
+    for letter_ in word[1:]:
+        if letter_.isdigit():
+            number += letter_
+    number = int(number)
+    return number * letter_position
 
 
-def uppercase_after(word: str):
-    pass
+def uppercase_after(word: str, front_result_: float):
+    letter = word[-1].lower()
+    letter_position = ord(letter) - 96
+    number = ""
+    for letter_ in word[:-1]:
+        if letter_.isdigit():
+            number += letter_
+    number = int(number)
+    return front_result_ - letter_position
 
 
-def lowercase_after(word: str):
-    pass
+def lowercase_after(word: str, front_result_: float):
+    letter = word[-1].lower()
+    letter_position = ord(letter) - 96
+    number = ""
+    for letter_ in word[:-1]:
+        if letter_.isdigit():
+            number += letter_
+    number = int(number)
+    return front_result_ + letter_position
+
 
 total = 0
+line = input().split()
 
-line = "A12b s17G".split()
-print(line)
-for word_ in line:
-    if word_[0].isupper():
-        total += uppercase_front(word_)
-    elif word_[0].islower():
-        total += lowercase_front(word_)
+for current_word in line:
+    front_result = 0
+    after_result = 0
+    if current_word[0].isupper():
+        front_result = uppercase_front(current_word[:-1])
+    elif current_word[0].islower():
+        front_result = lowercase_front(current_word[:-1])
 
-    if word_[-1].isupper():
-        total += uppercase_after(word_)
-    elif word_[-1].islower():
-        total += lowercase_after(word_)
+    if current_word[-1].isupper():
+        after_result = uppercase_after(current_word[1:], front_result)
+
+    elif current_word[-1].islower():
+        after_result = lowercase_after(current_word[1:], front_result)
+    total += after_result
 
 print(f"{total:.2f}")
