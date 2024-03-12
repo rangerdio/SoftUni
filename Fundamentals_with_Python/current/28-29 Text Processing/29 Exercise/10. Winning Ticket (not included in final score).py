d170de91aff2a8b06f05ff2eab8a5457185f067c
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-12 02:40:26 +0200

    pycharm update

--- Fundamentals_with_Python/current/28-29 Text Processing/29 Exercise/10. Winning Ticket (not included in final score).py
+++ Fundamentals_with_Python/current/28-29 Text Processing/29 Exercise/10. Winning Ticket (not included in final score).py
@@ -5,14 +5,41 @@ def isvalid(ticket):
         return False
 
 
-line = "$$$$$$$$$$$$$$$$$$$$, aabb  , th@@@@@@eemo@@@@@@ey"
+def is_winning(ticket):
+    winner = False
+    left = ticket[:10]
+    right = ticket[10:]
+    left_counter = 0
+    right_counter = 0
+    symbol = ""
+    for index, letter in enumerate(left):
+        if 10 > left.count(letter) >= 6:
+            left_counter = left.count(letter)
+            for letter_right in right:
+                if 10 > right.count(letter) >= 6:
+                    right_counter = right.count(letter)
+                    symbol = letter
+                    winner = True
+    if winner:
+        counter = left_counter if left_counter <= right_counter else right_counter
+        print(f'ticket "{ticket}" - {counter}{symbol}')
+
+
+line = input()
 tickets = line.split()
-print(tickets)
+tickets = "".join(tickets)
+tickets = tickets.split(",")
+
 
 for element in tickets:
     if not isvalid(element):
         print("invalid ticket")
     else:
-        print("valid")
+        if element[:10] == element[10:]:
+            print(f'ticket "{element}" - {10}{element[0]} Jackpot!')
+
+        elif element[:10] != element[10:]:
+             result = is_winning(element)
+
 
 
