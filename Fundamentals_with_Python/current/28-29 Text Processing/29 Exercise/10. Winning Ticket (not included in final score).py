93f14cde596f986ab1fcb6482e4d51b97e8b646b
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-12 19:28:30 +0200

    pycharm update

--- Fundamentals_with_Python/current/28-29 Text Processing/29 Exercise/10. Winning Ticket (not included in final score).py
+++ Fundamentals_with_Python/current/28-29 Text Processing/29 Exercise/10. Winning Ticket (not included in final score).py
@@ -20,7 +20,7 @@ def confirm_winner(left_d: dict, right_d: dict, ticket__):
         if value >= 6:
             right_symbol = key
             right_symbol_value = right_d[right_symbol]
-    print(right_symbol_value)
+    # print(right_symbol_value)
 
     if left_symbol and right_symbol:
         if left_symbol != right_symbol:
@@ -32,46 +32,33 @@ def confirm_winner(left_d: dict, right_d: dict, ticket__):
     elif not left_symbol or not right_symbol:
         print(f'"ticket "{element}" - no match"')
 
-
 def is_winning(ticket):
     winner = False
     left = ticket[:10]
     right = ticket[10:]
+
     left_counter = 1
     right_counter = 1
-    # left_symbol = ""
-    # right_symbol = ""
-    symbol = ""
+
     left_dict = {}
     right_dict = {}
+
     for index in range(len(left)):
         if index == len(left) - 1:
-            if left[index] == left[index - 1]:
-                left_dict[left[index]] = left_counter
-            else:
-                left_dict[left[index]] = left_counter
-                left_dict[left[index - 1]] = left_counter
-
+            left_dict[left[index]] = max(left_counter, left_dict.get(left[index], 0))
         elif left[index] == left[index + 1]:
             left_counter += 1
-
-        elif left[index] != left[index + 1]:
-            left_dict[left[index]] = left_counter
+        else:
+            left_dict[left[index]] = max(left_counter, left_dict.get(left[index], 0))
             left_counter = 1
 
     for index in range(len(right)):
         if index == len(right) - 1:
-            if right[index] == right[index - 1]:
-                right_dict[right[index]] = right_counter
-            else:
-                right_dict[right[index]] = right_counter
-                right_dict[right[index - 1]] = right_counter
-
+            right_dict[right[index]] = max(right_counter, right_dict.get(right[index], 0))
         elif right[index] == right[index + 1]:
             right_counter += 1
-
-        elif right[index] != right[index + 1]:
-            right_dict[right[index]] = right_counter
+        else:
+            right_dict[right[index]] = max(right_counter, right_dict.get(right[index], 0))
             right_counter = 1
 
     # print(left_dict)
