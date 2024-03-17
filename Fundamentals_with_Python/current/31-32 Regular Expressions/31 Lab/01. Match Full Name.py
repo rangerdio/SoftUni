cdc773a34415459b0535020521c8ec664708255f
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-17 12:05:01 +0200

    pycharm update

--- Fundamentals_with_Python/current/31-32 Regular Expressions/31 Lab/01. Match Full Name.py
+++ Fundamentals_with_Python/current/31-32 Regular Expressions/31 Lab/01. Match Full Name.py
@@ -1,5 +1,5 @@
 import re
 names = input()
-regex = r"\b[A-Z][a-z]* \b[A-Z][a-z]*\b"
-result = re.findall(regex, names)
+pattern = r'b[A-Z][a-z]+[ ][A-Z][a-z]+\b'
+result = re.findall(pattern, names)
 print(" ".join(result))
