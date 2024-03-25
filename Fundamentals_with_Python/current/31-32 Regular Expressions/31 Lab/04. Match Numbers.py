314c2608b20062d3138ff2ad5400ebc5617c1f13
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-25 18:44:36 +0200

    pycharm update

--- Fundamentals_with_Python/current/31-32 Regular Expressions/31 Lab/04. Match Numbers.py
+++ Fundamentals_with_Python/current/31-32 Regular Expressions/31 Lab/04. Match Numbers.py
@@ -1,20 +1,18 @@
+import re
+line = input()
+regex = r"(^|(?<=\s))-?([0]|[1-9][0-9]*)(\.\d+)?($|(?=\s))"
+
+result = re.finditer(regex, line)
+
+for element in result:
+    print(element.group(), end=" ")
+
+
 # import re
 # line = input()
 # regex = r"(^|(?<=\s))-?([0]|[1-9]*)(\.\d+)?(?=\s)"
 #
-# result = re.finditer(regex, line)
+# result = re.findall(regex, line)
 #
-# print(result)
-
 # for element in result:
-#     print(element.group(), end=" ")
-
-
-import re
-line = input()
-regex = r"(^|(?<=\s))-?([0]|[1-9]*)(\.\d+)?(?=\s)"
-
-result = re.findall(regex, line)
-
-for element in result:
-    print("".join(element), end=" ")
+#     print("".join(element), end=" ")
