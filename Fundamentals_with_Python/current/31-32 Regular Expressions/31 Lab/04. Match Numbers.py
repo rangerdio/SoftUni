ed014b0dc053772d54fa6bb9e13864808f5444ea
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-25 18:21:21 +0200

    pycharm update

--- Fundamentals_with_Python/current/31-32 Regular Expressions/31 Lab/04. Match Numbers.py
+++ Fundamentals_with_Python/current/31-32 Regular Expressions/31 Lab/04. Match Numbers.py
@@ -1,7 +1,20 @@
+# import re
+# line = "1 -1 1s 123 s-s -123 _55_ _f 123.456 -123.456 s-1.1 s2 -1- zs-2 s-3.5 00.5"
+# regex = r"(^|(?<=\s))-?([0]|[1-9]*)(\.\d+)?(?=\s)"
+#
+# result = re.finditer(regex, line)
+#
+# print(result)
+
+# for element in result:
+#     print(element.group(), end=" ")
+
+
 import re
 line = "1 -1 1s 123 s-s -123 _55_ _f 123.456 -123.456 s-1.1 s2 -1- zs-2 s-3.5 00.5"
 regex = r"(^|(?<=\s))-?([0]|[1-9]*)(\.\d+)?(?=\s)"
 
 result = re.findall(regex, line)
+
 for element in result:
-    print(element, end=" ")
+    print("".join(element), end=" ")
