1012ba4df7fa488700b950aeea24c770ca6aaef8
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-25 18:54:12 +0200

    pycharm update

--- Fundamentals_with_Python/current/31-32 Regular Expressions/31 Lab/04. Match Numbers.py
+++ Fundamentals_with_Python/current/31-32 Regular Expressions/31 Lab/04. Match Numbers.py
@@ -6,13 +6,3 @@ result = re.finditer(regex, line)
 
 for element in result:
     print(element.group(), end=" ")
-
-
-# import re
-# line = input()
-# regex = r"(^|(?<=\s))-?([0]|[1-9]*)(\.\d+)?(?=\s)"
-#
-# result = re.findall(regex, line)
-#
-# for element in result:
-#     print("".join(element), end=" ")
