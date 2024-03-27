5fca1e681c8b40585b204e5d46366513e836ce1e
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-27 21:22:39 +0200

    pycharm update

--- Fundamentals_with_Python/current/31-32 Regular Expressions/32 Exercise/04. Extract Emails.py
+++ Fundamentals_with_Python/current/31-32 Regular Expressions/32 Exercise/04. Extract Emails.py
@@ -1,6 +1,6 @@
 import re
 text = input()
-pattern = r'\b[A-Za-z0-9._+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+pattern = r'\s[A-Za-z0-9][A-Za-z0-9.,_-]*@[A-Za-z0-9][A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 matches = re.findall(pattern, text)
 for match in matches:
     print(match)
