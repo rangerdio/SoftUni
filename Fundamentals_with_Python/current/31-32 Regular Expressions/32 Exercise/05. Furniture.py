a1892305fd98bbcb3739f3b792e6d911307bb363
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-03-29 09:23:49 +0200

    pycharm update

--- Fundamentals_with_Python/current/31-32 Regular Expressions/32 Exercise/05. Furniture.py
+++ Fundamentals_with_Python/current/31-32 Regular Expressions/32 Exercise/05. Furniture.py
@@ -2,12 +2,21 @@ import re
 items = []
 total_cost = 0
 
-current_item = input()
-while current_item != "Purchase":
+
+while True:
+    current_item = input()
+    if current_item == "Purchase":
+        break
     pattern = r''
-    furniture_name, price, quantity = re.finditer(pattern, current_item)
-    total_cost += price * quantity
-    items.append(total_cost)
+    # furniture_name, price, quantity = re.finditer(pattern, current_item)
+    furniture_name = re.findall(r'((?<=>>)[A-Za-z]+)(?=<<)', current_item)
+    price = re.findall(r'(?<=<<)\d+\.?\d*(?=!)', current_item)
+    quantity = re.findall(r'(?<=!)\d+$', current_item)
+    # print(furniture_name, price, quantity)
+    if furniture_name and price and quantity:
+        price, quantity, furniture_name = float(price[0]), int(quantity[0]), furniture_name[0]
+        total_cost += price * quantity
+        items.append(furniture_name)
 
 print("Bought furniture:")
 for item in items:
