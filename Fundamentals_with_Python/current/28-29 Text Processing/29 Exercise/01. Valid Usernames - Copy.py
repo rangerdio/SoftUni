81a8a9ce32892290524b34aaaa8e9f0f3242472c
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-04-02 21:57:03 +0300

    pycharm update

---
+++ Fundamentals_with_Python/current/28-29 Text Processing/29 Exercise/01. Valid Usernames - Copy.py
@@ -0,0 +1,18 @@
+valid_len = []
+valid_usernames = []
+usernames = input().split(", ")
+for username in usernames:
+    if not (len(username) < 3 or len(username) > 16):
+        valid_len.append(username)
+
+
+for username in valid_len:
+    isvalid = True
+    for ch in username:
+        if not (ch.isalnum() or ch == "-" or ch == "_"):
+            isvalid = False
+    if isvalid:
+        valid_usernames.append(username)
+
+for asd in valid_usernames:
+    print(asd)
