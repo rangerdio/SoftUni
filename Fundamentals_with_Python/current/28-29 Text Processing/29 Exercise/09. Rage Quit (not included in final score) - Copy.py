da076f419c0978b9cc6fa24979f5fa4f4d2bc059
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-04-04 10:50:04 +0300

    pycharm update

--- Fundamentals_with_Python/current/28-29 Text Processing/29 Exercise/09. Rage Quit (not included in final score) - Copy.py
+++ Fundamentals_with_Python/current/28-29 Text Processing/29 Exercise/09. Rage Quit (not included in final score) - Copy.py
@@ -12,15 +12,20 @@ for index in range(len(line)):
     else:  # is digit
         number_string += line[index]
 
-        if index + 1 > len(line):
-            if line[index + 1].isdigit():
-                continue
-        else:  # number is gathered
+        if index + 1 >= len(line):
             result_line += word.upper() * int(number_string)
             word = ""
             number_string = ""
+        else:  # number is gathered
+            if line[index + 1].isdigit():
+                continue
+            result_line += word.upper() * int(number_string)
+            word = ""
+            number_string = ""
+
 for letter in result_line:
     unique[letter] = 1
+
 
 print(f"Unique symbols used: {len(unique)}")
 print(result_line)
