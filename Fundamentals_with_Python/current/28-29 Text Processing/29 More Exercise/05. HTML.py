b4e970c846281ac71e8daf3d962faebd82b4ee48
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-04-04 21:14:59 +0300

    pycharm update

--- Fundamentals_with_Python/current/28-29 Text Processing/29 More Exercise/05. HTML.py
+++ Fundamentals_with_Python/current/28-29 Text Processing/29 More Exercise/05. HTML.py
@@ -1,3 +1,7 @@
+def add_comment(comment_: str):
+    return f"<div>\n    {comment_}\n</div>"
+
+
 title = input()
 content = input()
 comments = []
@@ -5,3 +9,9 @@ while True:
     comment = input()
     if comment == "end of comments":
         break
+    comments.append(comment)
+
+print(f"<h1>\n    {title}\n</h1>")
+print(f"<article>\n    {content}\n</article>")
+for comment in comments:
+    print(add_comment(comment))
