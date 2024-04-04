468ac6bdd7a15ca0c3b404018af3fe1efe85fc49
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-04-04 11:36:26 +0300

    pycharm update

--- Fundamentals_with_Python/current/28-29 Text Processing/29 Exercise/10. Winning Ticket (not included in final score) - Copy.py
+++ Fundamentals_with_Python/current/28-29 Text Processing/29 Exercise/10. Winning Ticket (not included in final score) - Copy.py
@@ -1,18 +1,26 @@
-def analyse(ticket_: str):
+def analyse(ticket: str):
+    left_side = ticket[0:10]
+    right_side = ticket[10:]
+    if "@" or "#" or "$" or "^" in ticket:
+        if left_side == right_side:
+            return ["jackpot", len(left_side), left_side[0]]
+        else:
+            return [1,2,3]
 
-    return asd
 
+# ticket_s = [ticket_.strip() for ticket_ in input().split(",")]
+ticket_s = [ticket_.strip() for ticket_ in "$$$$$$$$$$$$$$$$$$$$, aabb  , th@@@@@@eemo@@@@@@ey".split(",")]
 
-# tickets = [ticket.strip() for ticket in input().split(",")]
-tickets = [ticket.strip() for ticket in "$$$$$$$$$$$$$$$$$$$$, aabb  , th@@@@@@eemo@@@@@@ey".split(",")]
+for ticket_ in ticket_s:
+    if len(ticket_) != 20:
+        print("invalid ticket_")
+    else:  # ticket_ is valid
+        analyse_ticket = analyse(ticket_)
+        # print(analyse_ticket)
 
-for ticket in tickets:
-    if len(ticket) != 20:
-        print("invalid ticket")
-    else:  # ticket is valid
-        if analyse(ticket)[0] == "jackpot":
-            print(f"ticket {ticket} - {analyse(ticket)[1]}{analyse(ticket)[2]} Jackpot!")
-        elif analyse(ticket)[0] == "winning":
-            print(f"ticket {ticket} - {analyse(ticket)[1]}{analyse(ticket)[2]}")
-        elif analyse(ticket)[0] == "no_match":
-            print(f"ticket {ticket} - no match")
+        if analyse_ticket[0] == "jackpot":
+            print(f"ticket {ticket_} - {analyse_ticket[1]} {analyse_ticket[2]} Jackpot!")
+        elif analyse_ticket[0] == "winning":
+            print(f"ticket {ticket_} - {analyse_ticket[1]}{analyse_ticket[2]}")
+        elif analyse(ticket_)[0] == "no_match":
+            print(f"ticket {ticket_} - no match")
