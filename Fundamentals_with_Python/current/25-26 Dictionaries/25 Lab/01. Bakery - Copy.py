8f11fedc490a86c2e7724158cda5f426d261bafd
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-04-04 22:38:26 +0300

    pycharm update

--- Fundamentals_with_Python/current/25-26 Dictionaries/25 Lab/01. Bakery - Copy.py
+++ Fundamentals_with_Python/current/25-26 Dictionaries/25 Lab/01. Bakery - Copy.py
@@ -1,5 +1,5 @@
 line = input().split(" ")
 bakery = {}
 for index in range(0, len(line), 2):
-    bakery[line[index]] = line[index + 1]
+    bakery[line[index]] = int(line[index + 1])
 print(bakery)
