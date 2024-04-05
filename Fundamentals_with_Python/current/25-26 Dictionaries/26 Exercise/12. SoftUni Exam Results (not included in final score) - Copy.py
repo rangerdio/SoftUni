b48a280e78e7654a009af4ddbaf3084bbb58eac9
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-04-06 01:20:08 +0300

    pycharm update

---
+++ Fundamentals_with_Python/current/25-26 Dictionaries/26 Exercise/12. SoftUni Exam Results (not included in final score) - Copy.py
@@ -0,0 +1,38 @@
+def populate_dictionary(results: dict, submissions: dict, user: str, lang: str, pts: str):
+    if user not in results.keys():      # user have no results
+        results[user] = pts
+
+        if lang not in submissions:             # language have no submissions
+            submissions[lang] = 1
+        else:                                   # language already have some submissions
+            submissions[lang] += 1
+    else:                               # user already have some results
+        if pts > results[user]:
+            results[user] = pts
+
+        if lang not in submissions:             # language have no submissions
+            submissions[lang] = 1
+        else:                                   # language already have some submissions
+            submissions[lang] += 1
+    return results, submissions
+
+
+user_results = {}
+submissions_ = {}
+
+while True:
+    line = input().split("-")
+    if line[0] == "exam finished":
+        break
+    if line[1] == "banned":
+        del user_results[line[0]]
+    else:
+        username, language, points = line[0], line[1], line[2]
+        user_results, submissions_ = populate_dictionary(user_results, submissions_, username, language, points)
+
+print("Results:")
+for username, points in user_results.items():
+    print(f'{username} | {points}')
+print('Submissions:')
+for language, submission_count in submissions_.items():
+    print(f'{language} - {submission_count}')
