cf0e859905c1f98d18c778e19c9fb809cedc8b37
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-04-05 09:45:46 +0300

    pycharm update

--- Fundamentals_with_Python/current/25-26 Dictionaries/25 Lab/04. Students - Copy.py
+++ Fundamentals_with_Python/current/25-26 Dictionaries/25 Lab/04. Students - Copy.py
@@ -6,6 +6,8 @@ while True:
         break
     dictionary[data[1]] = [{"name": data[0]}, {"course": data[2]}]
 
+course_to_search = course_to_search.replace("_", " ")
+
 for student_id, results in dictionary.items():
     if course_to_search in results[1].values():
         print(f"{results[0]['name']} - {student_id}")
