b5f4ae1e21b4453b38992270a194c4128a070553
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-04-06 20:31:34 +0300

    pycharm update

---
+++ Fundamentals_with_Python/current/Final_Exam/Final 3/03. Need for Speed III_Problem Description.py
@@ -0,0 +1,40 @@
+def drive(cars_: dict, car_: str, distance_: int, fuel_: int):
+    return cars_
+
+
+def refuel(cars_: dict, car_: str, fuel_: int):
+    return cars_
+
+
+def revert(cars_: dict, car_: str, kilometers: int):
+    return cars_
+
+
+cars = {}
+n = int(input())
+for i in range(n):
+    line = input().split("|")
+    car, mileage, fuel = line[0], int(line[1]), int(line[2])
+    cars[car] = {"mileage": mileage, "fuel": fuel}
+print(cars)
+
+while True:
+    line = input()
+    if line == "Stop":
+        break
+    command, car = line[0], line[1]
+
+    if command == "Drive":
+        distance = int(line[2])
+        fuel = int(line[3])
+        cars = drive(cars, car, distance, fuel)
+    elif command == "Refuel":
+        fuel = int(line[2])
+        cars = refuel(cars, car, fuel)
+    elif command == "Revert":
+        kilometers = int(line[2])
+        cars = revert(cars, car, kilometers)
+print(cars)
+
+for car, car_data in cars.items():
+    print(f'{car} -> Mileage: {car_data["mileage"]} kms, Fuel in the tank: {car_data["fuel"]}')
