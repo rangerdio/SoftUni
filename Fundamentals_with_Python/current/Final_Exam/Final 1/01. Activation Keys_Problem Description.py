6d4259e726cb666122d23ceee5e283632b2e0af3
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-04-07 20:31:18 +0300

    pycharm update

--- Fundamentals_with_Python/current/Final_Exam/Final 1/01. Activation Keys_Problem Description.py
+++ Fundamentals_with_Python/current/Final_Exam/Final 1/01. Activation Keys_Problem Description.py
@@ -1,23 +1,37 @@
 raw_key = input()
-activation_key = ''
+activation_key = raw_key
 
 while True:
     line = input().split(">>>")
     if line[0] == "Generate":
         break
-    command = [0]
+    command = line[0]
 
     if command == "Contains":
         substring = line[1]
-        pass
+        if substring in activation_key:
+            print(f'{activation_key} contains {substring}".')
+        else:
+            print('Substring not found!')
+
     elif command == "Flip":
+        command_letters = ""
         letter_types = line[1]
-        start_index = line[2]
-        end_index = line[3]
-        pass
+        start_index = int(line[2])
+        end_index = int(line[3])
+
+        if letter_types == "Upper":
+            activation_key = activation_key[:start_index] + \
+                             activation_key[start_index + 1: end_index - 1].upper() + activation_key[end_index:]
+        else:
+            activation_key = activation_key[:start_index] + \
+                             activation_key[start_index + 1: end_index - 1].lower() + activation_key[end_index:]
+        print(activation_key)
     else:   # else command is Slice
-        start_index = line[2]
-        end_index = line[3]
-        pass
+        print("asd")
+        start_index = int(line[1])
+        end_index = int(line[2])
+        activation_key = activation_key[:start_index] + activation_key[end_index:]
+        print(activation_key)
 
 print(f'Your activation key is: {activation_key}')
