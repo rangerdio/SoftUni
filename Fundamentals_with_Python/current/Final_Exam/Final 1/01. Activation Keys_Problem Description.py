92dc19b912c7568a20e2db42c9d9e2b3d35e0ef5
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-04-07 21:32:13 +0300

    pycharm update

--- Fundamentals_with_Python/current/Final_Exam/Final 1/01. Activation Keys_Problem Description.py
+++ Fundamentals_with_Python/current/Final_Exam/Final 1/01. Activation Keys_Problem Description.py
@@ -1,37 +1,39 @@
-raw_key = input()
-activation_key = raw_key
+key = input()
 
 while True:
     line = input().split(">>>")
     if line[0] == "Generate":
         break
     command = line[0]
-
     if command == "Contains":
         substring = line[1]
-        if substring in activation_key:
-            print(f'{activation_key} contains {substring}".')
+        if substring in key:
+            print(f'{key} contains {substring}".')
         else:
             print('Substring not found!')
 
     elif command == "Flip":
-        command_letters = ""
         letter_types = line[1]
         start_index = int(line[2])
         end_index = int(line[3])
 
         if letter_types == "Upper":
-            activation_key = activation_key[:start_index] + \
-                             activation_key[start_index + 1: end_index - 1].upper() + activation_key[end_index:]
+            key = key[:start_index] + key[start_index: end_index].upper() + key[end_index:]
         else:
-            activation_key = activation_key[:start_index] + \
-                             activation_key[start_index + 1: end_index - 1].lower() + activation_key[end_index:]
-        print(activation_key)
-    else:   # else command is Slice
-        print("asd")
+            key = key[:start_index] + key[start_index: end_index].lower() + key[end_index:]
+        print(key)
+
+    elif command == "Slice":
         start_index = int(line[1])
         end_index = int(line[2])
-        activation_key = activation_key[:start_index] + activation_key[end_index:]
-        print(activation_key)
+        key = key[:start_index] + key[end_index:]
+        # new_key = ""
+        # for index in range(len(key)):
+        #     if index < start_index:
+        #         new_key += key[index]
+        #     if index >= end_index:
+        #         new_key += key[index]
+        # key = new_key
+        print(key)
 
-print(f'Your activation key is: {activation_key}')
+print(f'Your activation key is: {key}')
