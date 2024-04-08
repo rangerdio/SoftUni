d820698bd2fc33a6ed3121a6b062254706a5db79
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-04-08 09:42:07 +0300

    pycharm update

--- Fundamentals_with_Python/current/Final_Exam/Final 2/02. Emoji Detector_Problem Description.py
+++ Fundamentals_with_Python/current/Final_Exam/Final 2/02. Emoji Detector_Problem Description.py
@@ -1,23 +1,30 @@
 import re
+
 text = input()
 
-pattern = f''
-matches = re.finditer(pattern, text)
+pattern_emo = r'(::|\*{2})([A-Z]{1}[a-z]{2,})\1'
+matches = list(re.finditer(pattern_emo, text))
+
+pattern_threshold = r'(\d)'
+matches_threshold = (re.findall(pattern_threshold, text))
+threshold = 1
+for match in matches_threshold:
+    threshold *= int(match)
 
 emojis = []
-for emoji in matches:
-    emojis.append(emoji.group(2))
+for match in matches:
+    emojis.append(f'{match.group(1)}{match.group(2)}{match.group(1)}')
 
-cool_threshold = 0
-coolness_data = {}
+cool_emos = []
 for emoji in emojis:
     emoji_coolness = 0
     for letter in emoji:
         if letter.isalpha():
             emoji_coolness += ord(letter)
-    cool_threshold += emoji_coolness
+    if emoji_coolness >= threshold:
+        cool_emos.append(emoji)
 
-print(f'Cool threshold: {cool_threshold}')
+print(f'Cool threshold: {threshold}')
 print(f'{len(emojis)} emojis found in the text. The cool ones are:')
-for emoji in emojis:
-    print(emoji)
+for emo in cool_emos:
+    print(emo)
