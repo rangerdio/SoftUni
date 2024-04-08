8d402f3efb3422fa6be1d4a4bcb48b2550230536
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-04-08 07:53:27 +0300

    pycharm update

--- Fundamentals_with_Python/current/Final_Exam/Final 2/02. Ad Astra_Problem Description.py
+++ Fundamentals_with_Python/current/Final_Exam/Final 2/02. Ad Astra_Problem Description.py
@@ -1,17 +1,16 @@
 import re
 text = input()
-pattern = r'(\||#)([a-zA-z\s]*)\1(\d{2}\/\d{2}\/\d{2})\1((?:[0-9]\d{0,3}|10000))\1'
-matches = re.finditer(pattern, text)
+pattern = r'(\||#)([a-zA-Z\s]+)\1(\d{2}\/\d{2}\/\d{2})\1((?:[0-9]\d{0,3}|10000))\1'
+matches = list(re.finditer(pattern, text))
 
 foods = {}
 total_cal = 0
 for match in matches:
-    food = match[2]
-    expiration = match[3]
     cal = int(match[4])
-    foods[food] = {"expiration": expiration, "calories": cal}
     total_cal += cal
 print(f'You have food to last you for: {total_cal // 2000} days!')
 
-for food, data in foods.items():
-    print(f'Item: {food}, Best before: {data["expiration"]}, Nutrition: {data["calories"]}')
+# matches = re.finditer(pattern, text)
+for match in matches:
+    print(f'Item: {match.group(2)}, Best before: {match.group(3)}, Nutrition: {match.group(4)}')
+
