7bf9199f5872176ad3cdae726251bfe12a7562ac
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-04-08 08:27:16 +0300

    pycharm update

--- Fundamentals_with_Python/current/Final_Exam/Final 2/02. Destination Mapper_Problem Description.py
+++ Fundamentals_with_Python/current/Final_Exam/Final 2/02. Destination Mapper_Problem Description.py
@@ -1,12 +1,13 @@
 import re
 locations = input()
+pattern = r'(=|\/)([A-Z]{1}[A-Za-z]{2,})\1'
+valid_locations = list(re.finditer(pattern, locations))
 
-pattern = r''
-valid_locations = re.findall(pattern, locations)
-print(locations)
-
+locations = []
 points = 0
-for location in locations:
+for match in valid_locations:
+    location = match.group(2)
+    locations.append(location)
     points += len(location)
 
 print(f'Destinations: {", ". join(locations)}')
