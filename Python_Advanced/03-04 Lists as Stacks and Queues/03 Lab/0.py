87123c52435feba776124046a1123dd9c372d023
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-06-20 10:12:17 +0300

    pycharm update

--- Python_Advanced/03-04 Lists as Stacks and Queues/03 Lab/0.py
+++ Python_Advanced/03-04 Lists as Stacks and Queues/03 Lab/0.py
@@ -48,14 +48,14 @@ For Queue we "from collections import deque"
 # queue = deque()
 #
 # while True:
-#     name = input()
-#     if name == "Paid":
-#         while queue:
-#             print(queue.popleft())
-#
-#     elif name == "End":
-#         print(f"{len(queue)}, people remaining.")
-#         break
-#     else:
-#         queue.append(name)
+# #     name = input()
+# #     if name == "Paid":
+# #         while queue:
+# #             print(queue.popleft())
+# #
+# #     elif name == "End":
+# #         print(f"{len(queue)}, people remaining.")
+# #         break
+# #     else:
+# #         queue.append(name)
 
