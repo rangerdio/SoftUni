76bfd8425e6eeca65a6d690999413be30622d77d
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-06-28 09:31:05 +0300

    pycharm update

--- Python_Advanced/03-04 Lists as Stacks and Queues/04 Exercise/2.py
+++ Python_Advanced/03-04 Lists as Stacks and Queues/04 Exercise/2.py
@@ -19,32 +19,17 @@
 # print(*stack, sep=", ")
 
 stack = []
-map_functions = {
-    1: lambda x: stack.append(x[1]),
-    2: lambda x: stack.pop(x[1]) if stack else None,
+options = {
+    1: lambda x: stack.append(int(x[1])),
+    2: lambda x: stack.pop() if stack else None,
     3: lambda x: print(max(stack)) if stack else None,
     4: lambda x: print(min(stack)) if stack else None,
 }
 
 for _ in range(int(input())):
     data = input().split()
-    command = data[0]
-
-n = int(input())
-
-for _ in range(n):
-    query = input().split()
-    if query[0] == "2":
-        if stack:
-            stack.pop()
-    elif query[0] == "3":
-        if stack:
-            print(max(stack))
-    elif query[0] == "4":
-        if stack:
-            print(min(stack))
-    else:
-        stack.append(int((query[1])))
+    command = int(data[0])
+    options[command](data)
 
 stack.reverse()
 print(*stack, sep=", ")
