37c10d12f22a6cc917bce56fc1d220c94e9d6982
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-07-01 15:43:24 +0300

    pycharm update

--- Python_Advanced/03-04 Lists as Stacks and Queues/04 Exercise/3.py
+++ Python_Advanced/03-04 Lists as Stacks and Queues/04 Exercise/3.py
@@ -1,16 +1,32 @@
+# from collections import deque
+# food_amount = int(input())
+#
+# orders = deque(map(int, input().split()))
+# print(max(orders))
+#
+# while orders:
+#     current_order = orders.popleft()
+#     if food_amount - current_order >= 0:
+#         food_amount -= current_order
+#     else:
+#         print("Orders left:", current_order, *orders)
+#         break
+# else:
+#     print("Orders complete")
+
+
 from collections import deque
 food_amount = int(input())
 
 orders = deque(map(int, input().split()))
 print(max(orders))
 
-while orders:
-    current_order = orders.popleft()
-    if food_amount - current_order >= 0:
-        food_amount -= current_order
-    else:
-        print("Orders left:", current_order, end=" ")
-        print(*orders, sep=" ")
+for order in orders.copy():
+    if food_amount < order:
+        print(f"Orders left:", *orders)
         break
+    else:
+        food_amount -= order
+        orders.popleft()
 else:
     print("Orders complete")
