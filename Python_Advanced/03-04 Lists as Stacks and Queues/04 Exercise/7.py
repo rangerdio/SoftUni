f5c7ecd0b2282aaddde2778283f278dcde950039
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-07-05 20:32:25 +0300

    pycharm update

--- Python_Advanced/03-04 Lists as Stacks and Queues/04 Exercise/7.py
+++ Python_Advanced/03-04 Lists as Stacks and Queues/04 Exercise/7.py
@@ -1,17 +1,9 @@
 from collections import deque
-
-
-def calculate_time(start_time_data_: list, seconds_: int):
-    calc = start_time_data_[0] * 3600 + start_time_data_[1] * 60 + start_time_data_[2] + seconds_
-    end_h = calc // 3600
-    end_m = (calc % 3600) // 60
-    end_s = (calc % 3600) % 60
-    return f'[{end_h:02}:{end_m:02}:{end_s:02}]'
-
+from datetime import datetime, timedelta
 
 robots_data = input().split(";")
 robots = {robot.split("-")[0]: [int(robot.split("-")[1]), 0] for robot in robots_data}
-start_time_data = [int(element) for element in input().split(":")]
+time = datetime.strptime(input(), "%H:%M:%S")
 products = deque()
 
 while True:
@@ -20,17 +12,16 @@ while True:
         break
     products.append(product)
 
-
-cnt = 0
 while products:
-    cnt += 1
-    current_time = calculate_time(start_time_data, cnt)
+    time += timedelta(0, 1)
     current_product = products.popleft()
+
     free_robots = []
 
     for robot_name, data in robots.items():
         if data[1] != 0:
             robots[robot_name][1] -= 1
+
         if data[1] == 0:
             free_robots.append([robot_name, data])
 
@@ -41,4 +32,4 @@ while products:
     rob_name, value = free_robots[0]
     robots[rob_name][1] = value[0]
 
-    print(f"{rob_name} - {current_product} {current_time}")
+    print(f"{rob_name} - {current_product} [{time.strftime('%H:%M:%S')}]")
