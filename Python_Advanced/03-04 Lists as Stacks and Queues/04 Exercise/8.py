3f033c6e57201135df8326952206d8d79219aa74
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-07-06 16:37:12 +0300

    pycharm update

--- Python_Advanced/03-04 Lists as Stacks and Queues/04 Exercise/8.py
+++ Python_Advanced/03-04 Lists as Stacks and Queues/04 Exercise/8.py
@@ -1,5 +1,5 @@
 from collections import deque
-green = int(input())
+green_time = int(input())
 free_window = int(input())
 cars = deque()
 
@@ -10,12 +10,39 @@ while True:
     cars.append(car)
 
 time = 0
+ext_time = 0
+total_cars = 0
+current_queue = deque()
 while cars:
-    current_queue = deque()
+    is_crash = False
     current_car = cars.popleft()
 
     if current_car == "green":
-        pass
+        time = green_time
 
+        while current_queue:
+            time -= 1
+            current_model = current_queue.popleft()
+            if len(current_model) < time:
+                time -= len(current_model)
+                continue
+            elif len(current_model) == time:
+                time -= len(current_model)
+                break
+            else:
+                time += ext_time
+                current_char = ""
+                for i in range(time, 0, -1):
+                    current_char = current_model.popleft()
+                print('A crash happened!')
+                print(f'{current_car} was hit at {current_char}.')
+                is_crash = True
+                break
+    if is_crash:
+        break
     current_queue.append(current_car)
+    total_cars += 1
 
+if not cars:
+    print(f'Everyone is safe.')
+    print(f'{total_cars} total cars passed the crossroads.')
