417e62c240ebe15bef1d62e7deaa12af9f7a87c8
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-07-06 10:53:38 +0300

    pycharm update

--- Python_Advanced/03-04 Lists as Stacks and Queues/04 Exercise/6.py
+++ Python_Advanced/03-04 Lists as Stacks and Queues/04 Exercise/6.py
@@ -1,38 +1,3 @@
-# from collections import deque
-# is_balanced = True
-#
-# parentheses_deque = deque(input())
-#
-# if len(parentheses_deque) % 2 == 1:
-#     is_balanced = False
-#
-# while parentheses_deque:
-#     if not is_balanced:
-#         break
-#     left = parentheses_deque.popleft()
-#     right = parentheses_deque.pop()
-#
-#     if left == "(":
-#         if right == ")":
-#             continue
-#         else:
-#             is_balanced = False
-#             break
-#     elif left == "{":
-#         if right == "}":
-#             continue
-#         else:
-#             is_balanced = False
-#             break
-#     elif left == "[":
-#         if right == "]":
-#             continue
-#         else:
-#             is_balanced = False
-#             break
-#
-# print("YES") if is_balanced else print("NO")
-
 
 from collections import deque
 is_balanced = True
