ffa73849b306c39afa5d254d16525599f311a155
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-07-09 18:51:59 +0300

    pycharm update

--- Python_Advanced/03-04 Lists as Stacks and Queues/04 Exercise/9.py
+++ Python_Advanced/03-04 Lists as Stacks and Queues/04 Exercise/9.py
@@ -2,40 +2,31 @@ from collections import deque
 
 bullet_price = int(input())
 barrel_size = int(input())
-
 bullets = deque(list(map(int, input().split())))
 locks = deque(list(map(int, input().split())))
-
 intelligence = int(input())
-print(bullet_price)
-print(barrel_size)
-print(bullets)
-print(locks)
-print(intelligence)
-
 
 money_spend = 0
 counter = 0
 
-while bullets:
+while bullets and locks:
     current_bullet = bullets.pop()
     counter += 1
     money_spend += bullet_price
 
-    while locks:
-        current_lock = locks.copy().popleft()
+    current_lock = locks.copy().popleft()
 
-        if current_bullet >= current_lock:
-            locks.remove(current_lock)
-            print("Bang!")
-        else:
-            print("Ping!")
+    if current_bullet < current_lock:
+        locks.remove(current_lock)
+        print("Bang!")
+    else:
+        print("Ping!")
 
     if bullets and counter == barrel_size:
         counter = 0
         print("Reloading!")
 
 if not locks:
-    print(f"{len(bullets)} bullets left. Earned #{intelligence - money_spend}")
+    print(f"{len(bullets)} bullets left. Earned ${intelligence - money_spend}")
 else:
     print(f"Couldn't get through. Locks left: {len(locks)}")
