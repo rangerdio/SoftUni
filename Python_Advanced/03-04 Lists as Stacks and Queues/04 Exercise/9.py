4526767874ffbd8f5b3a9db3d1fa94a4dff814a3
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-07-10 12:59:04 +0300

    pycharm update

--- Python_Advanced/03-04 Lists as Stacks and Queues/04 Exercise/9.py
+++ Python_Advanced/03-04 Lists as Stacks and Queues/04 Exercise/9.py
@@ -7,23 +7,23 @@ locks = deque(list(map(int, input().split())))
 intelligence = int(input())
 
 money_spend = 0
-counter = 0
+bullet_counter = 0
 
 while bullets and locks:
     current_bullet = bullets.pop()
-    counter += 1
+    bullet_counter += 1
     money_spend += bullet_price
 
     current_lock = locks.copy().popleft()
 
-    if current_bullet <= current_lock:
+    if current_lock >= current_bullet:
         locks.remove(current_lock)
         print("Bang!")
     else:
         print("Ping!")
 
-    if bullets and counter == barrel_size:
-        counter = 0
+    if bullets and bullet_counter == barrel_size:
+        bullet_counter = 0
         print("Reloading!")
 
 if not locks:
