3c6bad93a9be7129103ee1de8fafc26509365b12
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-07-13 19:52:33 +0300

    pycharm update

--- Python_Advanced/03-04 Lists as Stacks and Queues/04 Exercise/8.py
+++ Python_Advanced/03-04 Lists as Stacks and Queues/04 Exercise/8.py
@@ -1,52 +1,92 @@
-# not working
+# # not working
+#
+#
+# from collections import deque
+# green_time = int(input())
+# free_window = int(input())
+# cars = deque()
+#
+# while True:
+#     car = input()
+#     if car == "END":
+#         break
+#     cars.append(car)
+#
+# time = 0
+# ext_time = 0
+# total_cars = 0
+# current_queue = deque()
+# while cars:
+#     is_crash = False
+#     current_car = cars.popleft()
+#
+#     if current_car == "green":
+#         time = green_time
+#
+#         while current_queue:
+#             time -= 1
+#             current_model = current_queue.popleft()
+#             if len(current_model) < time:
+#                 time -= len(current_model)
+#                 continue
+#             elif len(current_model) == time:
+#                 time -= len(current_model)
+#                 break
+#             else:
+#                 time += ext_time
+#                 current_char = ""
+#                 for i in range(time, 0, -1):
+#                     current_char = current_model[0]
+#                     current_model = current_model[1:]
+#                 print('A crash happened!')
+#                 print(f'{current_car} was hit at {current_char}.')
+#                 is_crash = True
+#                 break
+#     if is_crash:
+#         break
+#     current_queue.append(current_car)
+#     total_cars += 1
+#
+# if not cars:
+#     print(f'Everyone is safe.')
+#     print(f'{total_cars} total cars passed the crossroads.')
 
 
 from collections import deque
-green_time = int(input())
-free_window = int(input())
-cars = deque()
 
+green_light_duration = int(input())
+free_window_duration = int(input())
+
+car_lane = deque()
 while True:
     car = input()
     if car == "END":
         break
-    cars.append(car)
+    car_lane.append(car)
 
-time = 0
-ext_time = 0
-total_cars = 0
+# print(car_lane)
+
+cars = deque()
 current_queue = deque()
-while cars:
-    is_crash = False
-    current_car = cars.popleft()
+while car_lane:
+    current_car = car_lane.popleft()
+    current_queue.append(current_car)
 
     if current_car == "green":
-        time = green_time
+        cars.append(current_queue)
+        current_queue = deque()
+        continue
 
-        while current_queue:
-            time -= 1
-            current_model = current_queue.popleft()
-            if len(current_model) < time:
-                time -= len(current_model)
-                continue
-            elif len(current_model) == time:
-                time -= len(current_model)
-                break
-            else:
-                time += ext_time
-                current_char = ""
-                for i in range(time, 0, -1):
-                    current_char = current_model[0]
-                    current_model = current_model[1:]
-                print('A crash happened!')
-                print(f'{current_car} was hit at {current_char}.')
-                is_crash = True
-                break
-    if is_crash:
-        break
-    current_queue.append(current_car)
-    total_cars += 1
+    if not car_lane:
+        cars.append(current_queue)
 
-if not cars:
-    print(f'Everyone is safe.')
-    print(f'{total_cars} total cars passed the crossroads.')
+print(cars)
+
+while cars:
+    current_loop = cars.popleft()
+
+    while current_loop:
+
+        current_car = cars.popleft()
+        if current_car == "green":
+            pass
