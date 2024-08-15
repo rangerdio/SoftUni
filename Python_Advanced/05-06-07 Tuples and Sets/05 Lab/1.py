122eb37a0d9ba8cf153a4620a956fb9f4134cf4e
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-08-15 10:49:39 +0300

    pycharm update

--- Python_Advanced/05-06-07 Tuples and Sets/05 Lab/1.py
+++ Python_Advanced/05-06-07 Tuples and Sets/05 Lab/1.py
@@ -3,9 +3,8 @@ numbers = tuple(float(f'{float(number):.1f}') for number in input().split(' '))
 dict_ = {}
 
 for number in numbers:
-    # if number not in dict_.keys():
-    #     dict_[number] = 0
-    dict_[number] = numbers.count(number)
+    if number not in dict_.keys():
+        dict_[number] = numbers.count(number)
 
 for key, value in dict_.items():
     print(f'{key} - {value} times')
