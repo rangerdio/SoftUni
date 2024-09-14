a78e77991eb7481e22f035e998963049feacc490
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-09-14 22:40:06 +0300

    pycharm update

--- Python_Advanced/05-06-07 Tuples and Sets/07 Exercise/6.py
+++ Python_Advanced/05-06-07 Tuples and Sets/07 Exercise/6.py
@@ -14,12 +14,17 @@ while strings:
         substring = strings.pop()
         if substring in available_colors:
             colors.append(substring)
+        else:
+            substring_cut = substring[0:-1:]
+            if len(substring_cut) != 0:
+                strings.append(substring_cut)
     else:
         # concatenate, check and append the lists
         first_substring = strings.popleft()
         second_substring = strings.pop()
         concatenate = first_substring + second_substring
         concatenate_2 = second_substring + first_substring
+
         if concatenate in available_colors:
             colors.append(concatenate)
         elif concatenate_2 in available_colors:
@@ -28,35 +33,39 @@ while strings:
             first_substring_cut = first_substring[0:-1:]
             second_substring_cut = second_substring[0:-1:]
 
-            insert_position = 0
-            if (len(strings) + 2) % 2 == 0:
-                insert_position = len(strings) // 2
-            else:
-                insert_position = len(strings) // 2 - 1
-
-            if len(first_substring_cut) != 0:
-                strings.insert(insert_position, first_substring_cut)
-            if len(second_substring_cut) != 0 and len(first_substring_cut) != 0:
-                strings.insert(insert_position + 1, second_substring_cut)
-            elif len(second_substring_cut) != 0 and len(first_substring_cut) == 0:
-                strings.insert(insert_position, second_substring_cut)
+            if first_substring_cut:
+                strings.insert(len(strings) // 2, first_substring_cut)
+            if second_substring_cut:
+                strings.insert(len(strings) // 2, second_substring_cut)
 
 
-color_map = {
-    'orange': ['red', 'yellow'],
-    'purple': ['red', 'blue'],
-    'green': ['yellow', 'blue'],
-}
-secondary = ['orange', 'purple', 'green']
 
-# print(colors)
-for color in secondary:
-    if color in colors:
-        needed_colors = color_map[color]
-        primary_color_1 = needed_colors[0]
-        primary_color_2 = needed_colors[1]
+# color_map = {
+#     'orange': ['red', 'yellow'],
+#     'purple': ['red', 'blue'],
+#     'green': ['yellow', 'blue'],
+# }
+# secondary = ['orange', 'purple', 'green']
+#
+# # print(colors)
+# for color in secondary:
+#     if color in colors:
+#         needed_colors = color_map[color]
+#         primary_color_1 = needed_colors[0]
+#         primary_color_2 = needed_colors[1]
+#
+#         if primary_color_1 not in colors or primary_color_2 not in colors:
+#             colors.remove(color)
 
-        if primary_color_1 not in colors or primary_color_2 not in colors:
-            colors.remove(color)
+for _ in range(len(colors)):    # checking if multiple times one collor is found to remove properly
+    if 'orange' in colors:
+        if 'red' not in colors or 'yellow' not in colors:
+            colors.remove('orange')
+    if 'purple' in colors:
+        if 'red' not in colors or 'blue' not in colors:
+            colors.remove('purple')
+    if 'green' in colors:
+        if 'yellow' not in colors or 'blue' not in colors:
+            colors.remove('green')
 
 print(colors)
