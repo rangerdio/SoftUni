6a352c954a3c01a61dcff24da98468d731e5e5a7
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-09-14 17:08:29 +0300

    pycharm update

--- Python_Advanced/05-06-07 Tuples and Sets/07 Exercise/5.py
+++ Python_Advanced/05-06-07 Tuples and Sets/07 Exercise/5.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-materials = deque([int(x) for x in input().split()])
+materials = [int(x) for x in input().split()]
 magic = deque([int(x) for x in input().split()])
 
 requirements = {
@@ -18,26 +18,32 @@ crafted = {
 }
 
 while materials and magic:
-    material_current = materials.pop()
-    magic_current = magic.popleft()
-    total_magic = material_current * magic_current
 
-    if total_magic in requirements.keys():
-        crafted[requirements[total_magic]] += 1
+    if materials[-1] == 0 and magic[0] == 0:
+        materials.pop()
+        magic.popleft()
+        continue
+    elif materials[-1] == 0:
+        materials.pop()
+        continue
+    elif magic[0] == 0:
+        magic.popleft()
+        continue
 
-    elif total_magic < 0:
-        materials.append(material_current + magic_current)
+    if materials[-1] * magic[0] in requirements.keys():
+        crafted[requirements[materials[-1] * magic[0]]] += 1
+        materials.pop()
+        magic.popleft()
 
-    elif total_magic > 0:
-        materials.append(material_current + 15)
+    elif materials[-1] * magic[0] < 0:
+        materials[-1] += magic[0]
+        ss = materials.pop()
+        materials.append(ss)
+        magic.popleft()
 
-    elif material_current == 0 and magic_current == 0:
-        pass
-    elif material_current == 0:
-        magic.insert(0, magic_current)
-
-    elif magic_current == 0:
-        materials.append(material_current)
+    elif materials[-1] * magic[0] > 0:
+        materials[-1] += 15
+        magic.popleft()
 
 if crafted['Doll'] > 0 and crafted['Wooden train'] > 0 or crafted['Teddy bear'] > 0 and crafted['Bicycle'] > 0:
     print('The presents are crafted! Merry Christmas!')
@@ -52,3 +58,59 @@ if magic:
 for key, value in crafted.items():
     if value > 0:
         print(f'{key}: {value}')
+
+#
+# from collections import deque
+#
+# materials = deque([int(x) for x in input().split()])
+# magic = deque([int(x) for x in input().split()])
+#
+# requirements = {
+#     150: 'Doll',
+#     250: 'Wooden train',
+#     300: 'Teddy bear',
+#     400: 'Bicycle',
+# }
+#
+# crafted = {
+#     'Bicycle': 0,
+#     'Doll': 0,
+#     'Teddy bear': 0,
+#     'Wooden train': 0,
+# }
+#
+# while materials and magic:
+#     material_current = materials.pop()
+#     magic_current = magic.popleft()
+#     total_magic = material_current * magic_current
+#
+#     if total_magic in requirements.keys():
+#         crafted[requirements[total_magic]] += 1
+#
+#     elif total_magic < 0:
+#         materials.append(material_current + magic_current)
+#
+#     elif total_magic > 0:
+#         materials.append(material_current + 15)
+#
+#     elif material_current == 0 and magic_current == 0:
+#         pass
+#     elif material_current == 0:
+#         magic.insert(0, magic_current)
+#
+#     elif magic_current == 0:
+#         materials.append(material_current)
+#
+# if crafted['Doll'] > 0 and crafted['Wooden train'] > 0 or crafted['Teddy bear'] > 0 and crafted['Bicycle'] > 0:
+#     print('The presents are crafted! Merry Christmas!')
+# else:
+#     print('No presents this Christmas!')
+#
+# if materials:
+#     print(f'Materials left: {", ".join([str(x) for x in reversed(materials)])}')
+# if magic:
+#     print(f'Magic left: {", ".join([str(x) for x in magic])}')
+#
+# for key, value in crafted.items():
+#     if value > 0:
+#         print(f'{key}: {value}')
