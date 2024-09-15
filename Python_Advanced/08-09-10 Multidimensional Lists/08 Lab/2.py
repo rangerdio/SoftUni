e9f1f3dc2c94e71b67b9266e86b8772024b39f45
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-09-15 19:30:14 +0300

    pycharm update

--- Python_Advanced/08-09-10 Multidimensional Lists/08 Lab/2.py
+++ Python_Advanced/08-09-10 Multidimensional Lists/08 Lab/2.py
@@ -1,10 +1,17 @@
+# rows = int(input())
+# matrix = []
+# for row in range(rows):
+#     sublist = [int(x) for x in input().split(', ')]
+#     even = []
+#     for element in sublist:
+#         if element % 2 == 0:
+#             even.append(element)
+#     matrix.append(even)
+# print(matrix)
+
 rows = int(input())
 matrix = []
 for row in range(rows):
-    sublist = [int(x) for x in input().split(', ')]
-    even = []
-    for element in sublist:
-        if element % 2 == 0:
-            even.append(element)
-    matrix.append(even)
+    sublist = [int(x) for x in input().split(', ') if int(x) % 2 == 0]
+    matrix.append(sublist)
 print(matrix)
