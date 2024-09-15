d15dc00a7fc37ba037f260083345a14c994bd1c7
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-09-15 17:14:29 +0300

    pycharm update

--- Python_Advanced/08-09-10 Multidimensional Lists/08 Lab/6.py
+++ Python_Advanced/08-09-10 Multidimensional Lists/08 Lab/6.py
@@ -1,11 +1,13 @@
 n = int(input())
 matrix = [[x for x in list(input())] for row in range(n)]
 symbol_ = input()
+is_found = False
 
 row_pos = 'empty'
 col_pos = "empty"
 for row in range(len(matrix)):
     if symbol_ in matrix[row]:
+        is_found = True
         row_pos = row
         col_pos = matrix[row].index(symbol_)
-print(f'{symbol_} does not occur in the matrix') if row_pos == 'empty' else print(f'({row_pos}, {col_pos})')
+print(f'{symbol_} does not occur in the matrix') if not is_found else print(f'({row_pos}, {col_pos})')
