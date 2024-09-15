578da48b3005297f26fc9726ce0cff685d0735e0
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-09-15 19:40:05 +0300

    pycharm update

--- Python_Advanced/08-09-10 Multidimensional Lists/08 Lab/3.py
+++ Python_Advanced/08-09-10 Multidimensional Lists/08 Lab/3.py
@@ -1,21 +1,20 @@
-rows = int(input())
-matrix = []
-for row in range(rows):
-    sublist = [int(x) for x in input().split(', ')]
-    matrix.append(sublist)
-
-flattened = []
-for sublist in matrix:
-    for element in sublist:
-        flattened.append(element)
-print(flattened)
+# rows = int(input())
+# matrix = []
+# for row in range(rows):
+#     sublist = [int(x) for x in input().split(', ')]
+#     matrix.append(sublist)
+#
+# flattened = []
+# for sublist in matrix:
+#     for element in sublist:
+#         flattened.append(element)
+# print(flattened)
 
 # flattened = [num for sublist in matrix for num in sublist]
-
 
 rows = int(input())
 matrix = []
 for row in range(rows):
     sublist = [int(x) for x in input().split(', ')]
     matrix.extend(sublist)
-print(flattened)
+print(matrix)
