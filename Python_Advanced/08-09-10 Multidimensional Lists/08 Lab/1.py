7d167169a8b3849faa507340c947599ccb0a7e21
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-09-15 18:52:47 +0300

    pycharm update

--- Python_Advanced/08-09-10 Multidimensional Lists/08 Lab/1.py
+++ Python_Advanced/08-09-10 Multidimensional Lists/08 Lab/1.py
@@ -1,12 +1,11 @@
 rows, cols = [int(x) for x in input().split(', ')]
 matrix = []
+summ = 0
 
 for row in range(rows):
     sublist = [int(x) for x in input().split(', ')]
     matrix.append(sublist)
+    summ += sublist
 
-summ = 0
-for row in range(len(matrix)):
-    summ += sum(matrix[row])
 print(summ)
 print(matrix)
