747a4039de5d3b38ebdd2a3d8271d1279fc0ddd1
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-09-17 09:54:03 +0300

    pycharm update

---
+++ Python_Advanced/08-09-10 Multidimensional Lists/09 Exercise/5.py
@@ -0,0 +1,13 @@
+r, c = [int(x) for x in input().split()]
+
+start_letter_row_num = 97
+start_letter_cow_num = 96
+matrix = []
+for row in range(r):
+    sublist = []
+    start_letter_cow_num += 1
+
+    for col in range(c):
+        sublist.append(chr(start_letter_row_num + row) + chr(start_letter_cow_num + col) + chr(start_letter_row_num + row))
+    matrix.append(sublist)
+print(matrix)
