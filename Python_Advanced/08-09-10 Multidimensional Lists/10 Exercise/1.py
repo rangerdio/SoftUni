ae5f1e0468d0645b3964f802aed0a36cee569391
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-09-21 07:51:12 +0300

    pycharm update

--- Python_Advanced/08-09-10 Multidimensional Lists/10 Exercise/1.py
+++ Python_Advanced/08-09-10 Multidimensional Lists/10 Exercise/1.py
@@ -8,3 +8,7 @@ for i in range(len(ss) - 1, -1, - 1):
 
 for row in matrix:
     print(" ".join(row), end=" ")
+
+# ss = [[x for x in row.split() if x] for row in input().split("|") if row]
+# for i in range(len(ss) - 1, -1, -1):
+#     print(" ".join(ss[i]), end=" ")
