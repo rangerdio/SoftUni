7bd0b653d2b097e4fb82b9b0e0505ec3a36e399b
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-09-21 18:01:33 +0300

    pycharm update

--- Python_Advanced/08-09-10 Multidimensional Lists/10 Exercise/3.py
+++ Python_Advanced/08-09-10 Multidimensional Lists/10 Exercise/3.py
@@ -1,59 +1,47 @@
-# # rows = int(input())
-# # matrix = []
-# # cols = 0
-# # for row in range(rows):
-# #     sublist = list(input())
-# #     cols = len(sublist)
-# #     matrix.append(sublist)
-# rows = 5
-# cols = 5
-# matrix = [
-#           ['0', 'K', '0', 'K', '0'],
-#           ['K', '0', '0', '0', 'K'],
-#           ['0', '0', 'K', '0', '0'],
-#           ['K', '0', '0', '0', 'K'],
-#           ['0', 'K', '0', 'K', '0']
-#           ]
-#
-#
-# def find_knights(matrix_: matrix, ):
-#     knights_ = []
-#     for row in range(rows):
-#         for col in range(cols):
-#             if matrix[row][col] == 'K':
-#                 knights_.append([row, col])
-#     return knights_
-#
-#
-# def find_wins():
-#     return
-#
-#
-# knights = find_knights(matrix)
-# # print(knights)
-# # 0 1
-# koko = []
-# for knight in knights:
-#     knight_row = knight[0]
-#     knight_col = knight[1]
-#     moves = [[knight_row - 2, knight_col + 1], [knight_row - 1, knight_col + 2], [knight_row + 1, knight_col + 2], [knight_row + 2, knight_col + 1],
-#              [knight_row + 2, knight_col - 1], [knight_row + 1, knight_col - 2], [knight_row - 1, knight_col - 1], [knight_row - 2, knight_col - 2]]
-#     wins_current_knight = {}
-#     # wins_current_knight = {knight: [+1], }
-#     points = 0
-#     list_wins = []
-#     for move in moves:
-#         if move in knights:
-#             # print(knight)
-#             # print(move)
-#             # print(knights)
-#             # print(wins_current_knight)
-#
-#             points += 1
-#             list_wins.append(move)
-#     wins_current_knight[str(knight)] = [points, list_wins]
-#     koko.append(wins_current_knight)
-#
-# print(koko)
+rows = int(input())
+cols = rows
+matrix = [list(input()) for row in range(rows)]
+
+
+def find_knights(matrix_: matrix):
+    knights_ = []
+    for row_ in range(len(matrix_)):
+        for col_ in range(len(matrix_)):
+            if matrix_[row_][col_] == 'K':
+                knights_.append([row_, col_])
+    return knights_
+
+
+knights = find_knights(matrix)
+moves = [-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1]
+removed = 0
+
+while True:
+    max_hits = 0
+    max_knight = [0, 0]
+
+    for knight in knights:
+        current_knight_row = knight[0]
+        current_knight_col = knight[1]
+        hits = 0
+        
+        for move in moves:
+            dest_row = current_knight_row + move[0]
+            dest_col = current_knight_col + move[1]
+            if 0 <= dest_row < rows and 0 <= dest_col < cols and matrix[dest_row][dest_col] == 'K':
+                hits += 1
+
+        if hits > max_hits:
+            max_hits = hits
+            max_knight = [current_knight_row, current_knight_col]
+
+    if max_hits == 0:
+        break
+
+    knights.remove(max_knight)
+    matrix[max_knight[0]][max_knight[1]] = '0'
+    removed += 1
+print(removed)
+
 
 
