5eee42614b565cfc6afa6c2dbdae2dbd33dc6851
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-09-29 12:29:58 +0300

    pycharm update

---
+++ Python_Advanced/13-14 Error Handling/14 Exercise/4.py
@@ -0,0 +1,32 @@
+class MatrixContentError(Exception):
+    pass
+
+
+class MatrixSizeError(Exception):
+    pass
+
+
+def rotate_matrix(matrix):
+    matrix_length = len(matrix)
+
+    for i in range(matrix_length):
+        for j in range(i, matrix_length):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(matrix_length):
+        matrix[i].reverse()
+
+
+mtrx = []
+
+while True:
+    line = input().split()
+
+    if not line:
+        break
+    mtrx.append(line)
+
+rotate_matrix(mtrx)
+
+for row in mtrx:
+    print(*row, sep=' ')
