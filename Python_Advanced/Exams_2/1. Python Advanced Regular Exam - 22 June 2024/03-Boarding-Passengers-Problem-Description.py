103d3827e32bfc58a2041253d08771241209349b
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-09-30 13:36:58 +0300

    pycharm update

--- Python_Advanced/Exams_2/1. Python Advanced Regular Exam - 22 June 2024/03-Boarding-Passengers-Problem-Description.py
+++ Python_Advanced/Exams_2/1. Python Advanced Regular Exam - 22 June 2024/03-Boarding-Passengers-Problem-Description.py
@@ -1,39 +1,73 @@
-import copy
-
-
 def boarding_passengers(capacity, *args):
     boarded = {}
-    initial_capacity = capacity
-    args_list = []
-    for element in args:
-        args_list.append(list(element))
-    koko = copy.copy(args_list)
+    boarded_counter = 0
 
-    for group in args_list:
-        if group[0] <= capacity:
-            capacity -= group[0]
-            if group[1] not in boarded.keys():
-                boarded[group[1]] = 0
-            boarded[group[1]] += group[0]
-            koko.remove(group)
+    for group_size, group_name in args:
+        if group_size <= capacity:
+            capacity -= group_size
+            boarded_counter += group_size
+            if group_name not in boarded:
+                boarded[group_name] = 0
+            boarded[group_name] += group_size
 
     sort_boarded = sorted(boarded.items(), key=lambda x: (-x[1], x[0]))
+    total = sum(arg[0] for arg in args)
 
-    total_shipped = 0
-    message = ''
-    for group, group_size in sort_boarded:
-        total_shipped += group_size
+    result = "Boarding details by benefit plan:\n"
 
-    if capacity == 0 and total_shipped == initial_capacity and len(koko) == 0:
+    if boarded_counter == total:
         message = 'All passengers are successfully boarded!'
-    elif capacity == 0 and len(args_list) > 0:
+    elif capacity == 0:
         message = "Boarding unsuccessful. Cruise ship at full capacity."
-    elif capacity > 0:
+    else:
         message = f'Partial boarding completed. Available capacity: {capacity}.'
 
-    formatted_string = "Boarding details by benefit plan:\n" + \
-                       "\n".join(f'## {group}: {ppl} guests' for group, ppl in sort_boarded) + "\n" + f'{message}'
-    return formatted_string
+    for group_name, ppl in sort_boarded:
+        result += f'## {group_name}: {ppl} guests\n'
+
+    return result + message
+
+
+# import copy
+#
+#
+# def boarding_passengers(capacity, *args):
+#     boarded = {}
+#     initial_capacity = capacity
+#     args_list = []
+#     for element in args:
+#         args_list.append(list(element))
+#     koko = copy.copy(args_list)
+#
+#     for group in args_list:
+#         if group[0] <= capacity:
+#             capacity -= group[0]
+#             if group[1] not in boarded.keys():
+#                 boarded[group[1]] = 0
+#             boarded[group[1]] += group[0]
+#             koko.remove(group)
+#
+#     sort_boarded = sorted(boarded.items(), key=lambda x: (-x[1], x[0]))
+#
+#     total_shipped = 0
+#     message = ''
+#     for group, group_size in sort_boarded:
+#         total_shipped += group_size
+#
+#     if capacity == 0 and total_shipped == initial_capacity and len(koko) == 0:
+#         message = 'All passengers are successfully boarded!'
+#     elif capacity == 0 and len(args_list) > 0:
+#         message = "Boarding unsuccessful. Cruise ship at full capacity."
+#     elif capacity > 0:
+#         message = f'Partial boarding completed. Available capacity: {capacity}.'
+#
+#     result = 'Boarding details by benefit plan:\n'
+#     for group, ppl in sort_boarded:
+#         result += f'## {group}: {ppl} guests\n'
+#
+#     return result + message
+
+
 
 
 # Test input
