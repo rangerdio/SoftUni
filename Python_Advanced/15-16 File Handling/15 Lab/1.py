55933f25f73333511e09df2dbbb33fb2b1c18e63
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-01 08:39:01 +0300

    pycharm update

---
+++ Python_Advanced/15-16 File Handling/15 Lab/1.py
@@ -0,0 +1,10 @@
+# file = open('koko.txt')
+#
+# koko = file.read()
+# file.close()
+# print(koko)
+# print(file.close())
+import os
+
+print(os.path.abspath('koko.txt'))
+print(os.path.abspath('koko.txt'))
