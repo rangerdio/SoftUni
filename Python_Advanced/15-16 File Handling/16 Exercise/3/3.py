620f305999adae0f9d012cb03be969c10f0c197e
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-02 00:08:24 +0300

    update

---
+++ Python_Advanced/15-16 File Handling/16 Exercise/3/3.py
@@ -0,0 +1,14 @@
+while True:
+    data = input()
+    if data == "End":
+        break
+    command, filename, *args = data.split('-')
+
+    if command == "Create":
+        pass
+    elif command == "Add":
+        pass
+    elif command == "Replace":
+        pass
+    elif command == "Delete":
+        pass
