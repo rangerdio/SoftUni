7631e33f750f07f41e7d0aad922eec59abef43b8
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-02 18:30:32 +0300

    update

--- Python_Advanced/15-16 File Handling/16 Exercise/4/4.py
+++ Python_Advanced/15-16 File Handling/16 Exercise/4/4.py
@@ -11,7 +11,6 @@ def get_files(root_dir, files, counter=1):
         current_path = os.path.join(root_dir, current_element)
         if os.path.isfile(current_path):
             extension = os.path.splitext(current_path)[1]
-            # path = os.path.splitext(current_path)[0]
 
             if extension not in files.keys():
                 files[extension] = []
@@ -19,16 +18,14 @@ def get_files(root_dir, files, counter=1):
         else:
             get_files(current_path, files, counter - 1)
 
-    sorted_files = sorted(files.items(), key=lambda x: (x[0], x[1]))
+    sorted_files = sorted(files.items(), key=lambda x: x[0])
     return sorted_files
 
 
 r_dir = '../'
-# with open(f"{root_dir}"+'koko.txt', 'w') as my_root_file:
-#     my_root_file.close()
+# r_dir = 'E:\\'
 
-# print(get_files(r_dir, files_))
-for ext, filenames in get_files(r_dir, files_):
+for ext, filenames in get_files(r_dir, files_, 1):
     print(f'{ext}')
-    for filename in filenames:
+    for filename in sorted(filenames, key=lambda x: x):
         print(f'- - - {filename}')
