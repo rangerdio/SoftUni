7107ae115ad522fe25239f0e8f476c795b96e149
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-02 08:49:19 +0300

    update

--- Python_Advanced/15-16 File Handling/16 Exercise/3/3.py
+++ Python_Advanced/15-16 File Handling/16 Exercise/3/3.py
@@ -2,12 +2,22 @@ while True:
     data = input()
     if data == "End":
         break
+    # print(data)
     command, filename, *args = data.split('-')
-
+    # print(command, filename, *args)
     if command == "Create":
-        pass
+        with open(filename, 'w') as my_file:
+            my_file.write('')
     elif command == "Add":
-        pass
+        try:
+            with open(filename, 'a') as my_file:
+                my_file.write(f'{"".join(args)}\n')
+        except FileNotFoundError:
+            with open(filename, 'w') as my_file:
+                my_file.write(f'{"".join(args)}\n')
+
+
+
     elif command == "Replace":
         pass
     elif command == "Delete":
