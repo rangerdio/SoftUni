38844174776080642743f2eebef1dfadc4ebdfd5
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-03 15:54:26 +0300

    update

--- Python_Advanced/15-16 File Handling/Rename_project_files_folders.py
+++ Python_Advanced/15-16 File Handling/Rename_project_files_folders.py
@@ -6,48 +6,24 @@ def rename(root_dir, files, counter=1):
         return files
     for element in os.listdir(root_dir):
         path_current_element = os.path.join(root_dir, element)
-        print(path_current_element)
+        # print('a', root_dir)
         # print(path_current_element)
         if os.path.isfile(path_current_element):
             my_path = os.path.split(path_current_element)[0]
             my_name = os.path.split(path_current_element)[1]
-            my_new_name = my_name.replace(' ', '_')
-            my_new_new_name = my_new_name.replace('-', '_')
-            new_path = os.path.join(my_path, my_new_new_name)
-            os.rename(path_current_element, new_path)
-            print(f'rename {my_name} to  {my_new_new_name}')
+            if ' ' in my_name or '-' in my_name:
+                my_new_name = my_name.replace(' ', '_')
+                my_new_new_name = my_new_name.replace('-', '_')
+                new_path = os.path.join(my_path, my_new_new_name)
+                os.rename(path_current_element, new_path)
+                print(f'rename {my_name} to  {my_new_new_name}')
         else:
-            continue
-
-        # if os.path.isfile(path_current_element):
-        #     print('f', path_current_element)
-        #
-        # elif os.path.isdir(path_current_element):
-        #     print('d', path_current_element)
+            rename(path_current_element, counter - 1)
 
 
-    # try:
-    #     for current_element in os.listdir(root_dir):
-    #         current_path = os.path.join(root_dir, current_element)
-    #         if os.path.isfile(current_path):
-    #             files.append(current_path)
-    #             print(current_path)
-    #         else:
-    #             try:
-    #                 get_files(current_path, files, counter - 1)
-    #             except PermissionError:
-    #                 continue
-    # except (FileNotFoundError, PermissionError):
-    #     pass
-    # except Exception:
-    #     pass
-    #
-    # return files
-
-
-r_dir = 'G:\\My Drive\\Programming\\SoftUni\\Python_Advanced\\15-16 File Handling'
+r_dir = 'G:\\My Drive\\Programming\\SoftUni\\Python_Advanced\\'
 files_ = []
 
-rename(r_dir, 2)
+rename(r_dir, 4)
 
 # flie_path = os.path.join(r_dir, 'export.txt')
