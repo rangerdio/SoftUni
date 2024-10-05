b135c7da2b6db543b3e2dc6543b131a843fb1185
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-05 15:03:25 +0300

    update

---
+++ Python_Advanced/Exams_2/3._Python_Advanced_Exam_17_February_2024/03_Cookbook.py
@@ -0,0 +1,29 @@
+def cookbook(recipe_name, cuisine, *ingredients):
+    print(recipe_name, cuisine)
+    print(ingredients)
+    print('\n')
+    return
+
+
+print(cookbook(
+    ("Spaghetti Bolognese", "Italian", ["spaghetti", "tomato sauce", "ground beef"]),
+    ("Margherita Pizza", "Italian", ["pizza dough", "tomato sauce", "mozzarella"]),
+    ("Tiramisu", "Italian", ["ladyfingers", "mascarpone", "coffee"]),
+    ("Croissant", "French", ["flour", "butter", "yeast"]),
+    ("Ratatouille", "French", ["eggplant", "zucchini", "tomatoes"])
+))
+
+print(cookbook("Pad Thai",
+               "Thai",
+               ["rice noodles", "shrimp", "peanuts", "bean sprouts", "tamarind sauce"]))
+
+print(cookbook(
+    ("Spaghetti Bolognese", "Italian", ["spaghetti", "tomato sauce", "ground beef"]),
+    ("Margherita Pizza", "Italian", ["pizza dough", "tomato sauce", "mozzarella"]),
+    ("Tiramisu", "Italian", ["ladyfingers", "mascarpone", "coffee"]),
+    ("Croissant", "French", ["flour", "butter", "yeast"]),
+    ("Ratatouille", "French", ["eggplant", "zucchini", "tomatoes"]),
+    ("Sushi Rolls", "Japanese", ["rice", "nori", "fish", "vegetables"]),
+    ("Miso Soup", "Japanese", ["tofu", "seaweed", "green onions"]),
+    ("Guacamole", "Mexican", ["avocado", "tomato", "onion", "lime"])
+    ))
