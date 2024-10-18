bf1046402fc4c0b2b91b44a215e43b19d8766bb6
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-18 10:35:04 +0300

    py update

--- filtering/filter_maker.py
+++ filtering/filter_maker.py
@@ -5,7 +5,7 @@
 #   CSTA callid's
 #   SIP CallIds
 
-raw_data = '538814188,538814189 538814189 538814190 538814190 538814188 538814188 538814191 \
+raw_data = '5381418 5388189 5384189 5814190 5384190 8814188 5388141 5388141 5388141 \
 538814191 SEC11-610d397-710d397-1-yrDCLnlcNUIW c1d574cae7024e38 SEC11-610d397-710d397-1-4nK96z2x5fFZ \
 c55fa32041bc4314 SEC11-610d397-710d397-1-DZxse1RlD08g c5b796bceac7993a SEC11-610d397-710d397-1-F39y1y5R3x0a \
 c71047a099ac8e93 SEC11-610d397-710d397-1-BJF1dSVPXGCX c855ef523e1603c4 SEC11-610d397-710d397-1-kbb3qkZ053zx \
@@ -14,10 +14,57 @@ cba40c793566499f SDb0gk602--SCP-vm-cdf-onenet-pbxip01-P-1726750859287-cafa118c_b
 0300010000000000F920EC664FGE0000'.replace(',', ' ')
 # raw_data = raw_data.replace(',', ' ')
 raw_data_list = raw_data.split()
-print(raw_data_list)
+# print(raw_data_list)
 
-sip_ids, csta_ids, mgcp_ids = []
+sip_ids, csta_ids, csta_ids_2nd, mgcp_ids = [], [], [], []
+wireshark_filter = []
+
+
+def transform_sip(sip_ids):
+    transformed_sip_ids = []
+    for sip_id in sip_ids:
+        a, b = '(sip.Call-ID == "', '")'
+        transformed_sip_ids.append(a + sip_id + b)
+    return transformed_sip_ids
+
+
+def transform_csta(csta_ids):
+    transformed_csta_ids = []
+
+    for csta_id in csta_ids:
+        a, b = '(xml.cdata == "FF0', '")'
+        csta_id = a + csta_id[:-3] + b
+        transformed_csta_ids.append(csta_id)
+    return transformed_csta_ids
+
+
+def transform_csta_2nd(csta_ids_2nd):
+    transformed_csta_ids_2nd = []
+    for csta_id in csta_ids_2nd:
+        a, b = '(xml.cdata == "FF', '")'
+        csta_id = a + csta_id[2:] + b
+        transformed_csta_ids_2nd.append(csta_id)
+    return transformed_csta_ids_2nd
+
+
+def transform_mgcp_ids(mgcp_ids):
+    transformed_mgcp_ids = []
+    for mgcp_id in mgcp_ids:
+        a, b = '(mgcp.transid == "', '")'
+        transformed_mgcp_ids.append(a + mgcp_id + b)
+    return transformed_mgcp_ids
+
 
 for element in raw_data_list:
     if element.isnumeric() and len(element) == 7:
+        mgcp_ids.append(element)
+    elif element.endswith('-FF') and '000000000' in element:
+        csta_ids.append(element)
+    elif '000000000' in element and element.startswith('0'):
+        csta_ids_2nd.append(element)
+    else:
+        sip_ids.append(element)
 
+wireshark_filter = ' or '.join(transform_sip(sip_ids) + transform_csta(csta_ids) +
+                               transform_csta_2nd(csta_ids_2nd) + transform_mgcp_ids(mgcp_ids))
+print(wireshark_filter)
