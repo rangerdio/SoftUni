8c63e4f2c1a6362f43838999df00546e8e42b71d
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-23 13:23:52 +0300

    py update

---
+++ Python_OOP/03-04/04/8/trainer.py
@@ -0,0 +1,14 @@
+class Trainer:
+    def __init__(self, name: str):
+        self.name = name
+        self.pokemons = []
+
+    def add_pokemon(self, pokemon: Pokemon):
+        pass
+
+    def release_pokemon(self, pokemon_name: string):
+        pass
+
+    def trainer_data(self, ):
+        pass
+    
