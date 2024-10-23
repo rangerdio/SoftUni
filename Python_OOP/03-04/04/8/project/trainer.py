b75fc89134af9bbacd6191df4489eba0feb93314
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-23 20:16:35 +0300

    py update

--- Python_OOP/03-04/04/8/project/trainer.py
+++ Python_OOP/03-04/04/8/project/trainer.py
@@ -1,25 +1,27 @@
-from pokemon import Pokemon
+from typing import List
+
+from project.pokemon import Pokemon
 
 
 class Trainer:
     def __init__(self, name: str):
         self.name = name
-        self.pokemons = []
+        self.pokemons: List[Pokemon] = []
 
-    def add_pokemon(self, pokemon: Pokemon):
+    def add_pokemon(self, pokemon: Pokemon) -> str:
         if pokemon in self.pokemons:
             return "This pokemon is already caught"
         self.pokemons.append(pokemon)
         return f'Caught {Pokemon.pokemon_details(pokemon)}'
 
-    def release_pokemon(self, pokemon_name: str):
+    def release_pokemon(self, pokemon_name: str)  -> str:
         for pokemon in self.pokemons:
             if pokemon.name == pokemon_name:
                 self.pokemons.remove(pokemon)
                 return f'You have released {pokemon_name}'
         return f'Pokemon is not caught'
 
-    def trainer_data(self, ):
+    def trainer_data(self) -> str:
         result = f'Pokemon Trainer {self.name}\nPokemon count {len(self.pokemons)}\n'
         for pok in self.pokemons:
             result += f'- {Pokemon.pokemon_details(pok)}'
