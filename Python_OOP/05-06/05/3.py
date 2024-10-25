b6d44d5ced943adbe9b9fc25fede08a4fa00fd97
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-25 16:21:55 +0300

    py update

--- Python_OOP/05-06/05/3.py
+++ Python_OOP/05-06/05/3.py
@@ -1,17 +1,17 @@
-# class Circle:
-#     pi = 3.14
-#
-#     def __init__(self, radius: float):
-#         self.radius = radius
-#
-#     def set_radius(self, radius: float):
-#         self.radius = radius
-#
-#     def get_area(self, pi=pi):
-#         return self.radius ** 2 * pi
-#
-#     def get_circumference(self, pi=pi):
-#         return self.radius * 2 * pi
+class Circle:
+    pi = 3.14
+
+    def __init__(self, radius: float):
+        self.radius = radius
+
+    def set_radius(self, radius: float):
+        self.radius = radius
+
+    def get_area(self, pi=pi):
+        return self.radius ** 2 * pi
+
+    def get_circumference(self, pi=pi):
+        return self.radius * 2 * pi
 
 
 circle = Circle(10)
