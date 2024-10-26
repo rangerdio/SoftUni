ff2c0e8e0ac1972e62527477d715f3216f849351
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-26 09:42:21 +0300

    py update

---
+++ Python_OOP/05-06/06/2/project/2.py
@@ -0,0 +1,35 @@
+class Time:
+    max_hours = 23
+    max_minutes = 59
+    max_seconds = 59
+
+    def __init__(self, hours: int, minutes: int, seconds: int):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def set_time(self, hours, minutes, seconds):
+        self.hours, self.minutes, self.seconds = hours, minutes, seconds
+
+    def get_time(self):
+        return f'{self.hours:02}:{self.minutes:02}:{self.seconds:02}'
+
+    def next_second(self):
+        second = 1
+        if self.seconds + 1 == 60:
+            self.seconds = 0
+            self.minutes += 1
+            if self.minutes == 60:
+                self.minutes = 0
+                self.hours += 1
+                if self.hours == 24:
+                    self.hours = 0
+        return Time.get_time(self)
+
+
+time = Time(9, 30, 59)
+print(time.next_second())
+time = Time(10, 59, 59)
+print(time.next_second())
+time = Time(23, 59, 59)
+print(time.next_second())
