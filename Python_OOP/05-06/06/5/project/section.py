2c6e8f705d43c6febd3971152953073e9fa4c7b5
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-26 15:11:17 +0300

    py update

---
+++ Python_OOP/05-06/06/5/project/section.py
@@ -0,0 +1,18 @@
+class Section:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.tasks = []
+
+    def add_task(self, new_task: Task):
+        pass
+
+    def complete_task(self, task_name: str):
+        pass
+
+    def clean_section(self):
+        pass
+
+    def view_section(self):
+        pass
+
+
