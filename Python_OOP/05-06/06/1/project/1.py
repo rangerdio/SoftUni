98fd5696f5ecdcc3449ad3dfb5484f5ba2c9edfa
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-26 09:23:34 +0300

    py update

--- Python_OOP/05-06/06/1/project/1.py
+++ Python_OOP/05-06/06/1/project/1.py
@@ -9,12 +9,37 @@ class Vet:
         self.name = name
         self.animals = []
 
-    def register_animals(self):
-        pass
+    def register_animal(self, animal_name: str):
+        if Vet.space > 0:
+            self.animals.append(animal_name)
+            Vet.animals.append(animal_name)
+            Vet.space -= 1
+            return f"{animal_name} registered in the clinic"
+        else:
+            return "Not enough space"
 
-    def unregister_animal(self):
-        pass
+    def unregister_animal(self, animal_name: str):
+        if animal_name in Vet.animals:
+            Vet.animals.remove(animal_name)
+            Vet.space += 1
+            self.animals.remove(animal_name)
+            return f"{animal_name} unregistered successfully"
+        return f"{animal_name} not in the clinic"
 
     def info(self):
-        # animals_in_clinic = 0
-        # return f"{self.name} has {len(self.animals)} animals. {Vet.space - len(self.animals)} space left in clinic"
+        return f"{self.name} has {len(self.animals)} animals. {Vet.space} space left in clinic"
+
+
+peter = Vet("Peter")
+george = Vet("George")
+print(peter.register_animal("Tom"))
+print(george.register_animal("Cory"))
+print(peter.register_animal("Fishy"))
+print(peter.register_animal("Bobby"))
+print(george.register_animal("Kay"))
+print(george.unregister_animal("Cory"))
+print(peter.register_animal("Silky"))
+print(peter.unregister_animal("Molly"))
+print(peter.unregister_animal("Tom"))
+print(peter.info())
+print(george.info())
