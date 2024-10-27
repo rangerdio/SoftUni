dc331c27b182d0e7ccb9fbc2367efbc282b61fac
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-27 18:06:32 +0200

    py update

--- Python_OOP/05-06/06/6/project/guild.py
+++ Python_OOP/05-06/06/6/project/guild.py
@@ -16,14 +16,23 @@ class Guild:
         return f"Welcome player {player.name} to the guild {self.name}"
 
     def kick_player(self, player_name: str) -> str:
-        player = next((p for p in self.players if p.name == player_name), None)
-        if not player:
-            return f'Player {player_name} is not in the guild.'
-        player_name.guild = 'Unaffiliated'
-        self.players.remove(player)
-        return f"Player {player_name} has been removed from the guild."
+        # player = next((p for p in self.players if p.name == player_name), None)
+        # if not player:
+        #     return f'Player {player_name} is not in the guild.'
+        # player_name.guild = 'Unaffiliated'
+        # self.players.remove(player)
+        # return f"Player {player_name} has been removed from the guild."
+        for player in self.players:
+            if player.name == player_name:
+                player.guild = 'Unaffiliated'
+                self.players.remove(player)
+                return f"Player {player_name} has been removed from the guild."
 
     def guild_info(self) -> str:
-        result = f"Guild: {self.name}\n"
-        result += '\n'.join(current_player.player_info() for current_player in self.players)
-        return result
+        # result = f"Guild: {self.name}\n"
+        # result += '\n'.join(current_player.player_info() for current_player in self.players)
+        # return result
+        result = [f"Guild: {self.name}"]
+        for player in self.players:
+            result.append(player.player_info())
+        return '\n'.join(result)
