05f18f1cfddba1e34c0beb419cc873d31bc780a8
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-27 07:20:07 +0200

    py update

--- Python_OOP/05-06/06/5/project/section.py
+++ Python_OOP/05-06/06/5/project/section.py
@@ -1,11 +1,11 @@
-from project import task
+from typing import List
 from project.task import Task
 
 
 class Section:
     def __init__(self, name: str) -> None:
         self.name = name
-        self.tasks: list = []
+        self.tasks: List[Task] = []
 
     def add_task(self, new_task: Task) -> str:
         if new_task in self.tasks:
@@ -14,19 +14,27 @@ class Section:
         return f'Task {new_task.details()} is added to the section'
 
     def complete_task(self, task_name: str) -> str:
-        if task_name in self.tasks:
-            task_name.completed = True
+        # if task_name in self.tasks:
+        #     task_name.completed = True
+        #     return f'Completed task {task_name}'
+        # else:
+        #     return f'Task {task_name} is not found in the section {self.name}'
+        task_to_complete = next((current_task for current_task in self.tasks if current_task.name == task_name), None)
+        if task_to_complete:
+            task_to_complete[0].completed = True
             return f'Completed task {task_name}'
         else:
             return f'Task {task_name} is not found in the section {self.name}'
 
     def clean_section(self) -> str:
-        amount_removed_tasks = 0
-        for current_task in self.tasks:
-            if current_task.completed:
-                self.tasks.remove(current_task)
-                amount_removed_tasks += 1
-        return f'Cleared {amount_removed_tasks} tasks.'
+        total = len(self.tasks)
+        # amount_removed_tasks = 0
+        # for current_task in self.tasks:
+        #     if current_task.completed:
+        #         self.tasks.remove(current_task)
+        #         amount_removed_tasks += 1
+        self.tasks = [current_task for current_task in self.tasks if not current_task.completed]
+        return f'Cleared {total - len(self.tasks)} tasks.'
 
     def view_section(self) -> str:
         result = ''
