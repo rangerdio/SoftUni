3fc42ff39c064df0fd1ae61bd7557bc936d5ec56
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-27 18:36:30 +0200

    py update

--- Python_OOP/05-06/06/6/project/guild.py
+++ Python_OOP/05-06/06/6/project/guild.py
@@ -11,15 +11,15 @@ class Guild:
             return f"Player {player.name} is already in the guild."
         if player.guild != 'Unaffiliated':
             return f"Player {player.name} is in another guild."
-        player.guild = self.name
         self.players.append(player)
+        player.guild = self.name
         return f"Welcome player {player.name} to the guild {self.name}"
 
     def kick_player(self, player_name: str) -> str:
         for player in self.players:
             if player.name == player_name:
-                player.guild = 'Unaffiliated'
                 self.players.remove(player)
+                player.guild = 'Unaffiliated'
                 return f"Player {player_name} has been removed from the guild."
         return f'Player {player_name} is not in the guild.'
 
--- Python_OOP/05-06/06/6/project/player.py
+++ Python_OOP/05-06/06/6/project/player.py
@@ -13,9 +13,9 @@ class Player:
         return f'Skill {skill_name} added to the collection of the player {self.name}'
 
     def player_info(self) -> str:
-        dat = "\n".join(f'==={skill_name} - {skill_mana_cost}' for skill_name, skill_mana_cost in self.skills.items())
-        return f'Name: {self.name}\nGuild: {self.guild}\nHP: {self.hp}\nMP: {self.mp}\n{dat}'
-        # result = [f'Name: {self.name}', f'Guild: {self.guild}', f'HP: {self.hp}', f'MP: {self.mp}']
-        # for skill_name, skill_mana_cost in self.skills.items():
-        #     result.append(f'=== {skill_name} - {skill_mana_cost}')
-        # return '\n'.join(result)
+        # dat = "\n".join(f'==={skill_name} - {skill_mana_cost}' for skill_name, skill_mana_cost in self.skills.items())
+        # return f'Name: {self.name}\nGuild: {self.guild}\nHP: {self.hp}\nMP: {self.mp}\n{dat}'
+        result = [f'Name: {self.name}', f'Guild: {self.guild}', f'HP: {self.hp}', f'MP: {self.mp}']
+        for skill_name, skill_mana_cost in self.skills.items():
+            result.append(f'=== {skill_name} - {skill_mana_cost}')
+        return '\n'.join(result)
