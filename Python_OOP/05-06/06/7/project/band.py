83da5823e26e64213ef3bc7a82dd4d67ced203d6
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-28 11:18:32 +0200

    py update

--- Python_OOP/05-06/06/7/project/band.py
+++ Python_OOP/05-06/06/7/project/band.py
@@ -1,2 +1,28 @@
+from project.album import Album
+
+
 class Band:
-    pass
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.albums: list[Album] = []
+
+    def add_album(self, album: Album) -> str:
+        if album in self.albums:
+            return f'Band {self.name} already has {album.name} in their library.'
+        self.albums.append(album)
+        return f'Band {self.name} has added their newest album {album.name}.'
+
+    def remove_album(self, album_name: str) -> str:
+        try:
+            my_album = [album for album in self.albums if album.name == album_name][0]
+            if my_album.published:
+                return f'Album has been published. It cannot be removed.'
+            if my_album.name == album_name:
+                self.albums.remove(my_album)
+                return f'Album {my_album.name} has been removed.'
+        except IndexError:
+            return f'Album {album_name} is not found.'
+
+    def details(self) -> str:
+        band_details = f'Band {self.name}\n'
+        return band_details + '\n'.join(album.details() for album in self.albums)
