4e7b480f3acf6c4f21cc3d10cc4720d1660ecc60
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-29 07:32:03 +0200

    py update

---
+++ Python_OOP/05-06/06/8/project/library.py
@@ -0,0 +1,11 @@
+class Library:
+    def __init__(self):
+        self.books_available = None
+
+    pass
+
+    def get_book(self, param, param1, param2, user):
+        pass
+
+    def return_book(self, param, param1, user):
+        pass
---
+++ Python_OOP/05-06/06/8/project/registration.py
@@ -0,0 +1,9 @@
+class Registration:
+    def add_user(self, user, library):
+        pass
+
+    def remove_user(self, user, library):
+        pass
+
+    def change_username(self, param, param1, library):
+        pass
