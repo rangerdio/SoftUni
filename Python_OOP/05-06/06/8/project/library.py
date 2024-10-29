ec2a142d4f923125282ffbba7cc84a0435f74d67
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-29 09:05:54 +0200

    py update

--- Python_OOP/05-06/06/8/project/library.py
+++ Python_OOP/05-06/06/8/project/library.py
@@ -1,11 +1,19 @@
+from project.user import User
+
+
 class Library:
     def __init__(self):
-        self.books_available = None
+        self.user_records: list[User] = []
+        self.books_available: dict[str, list[str]] = {}
+        self.rented_books: dict[str, dict[str, int]] = {}
 
-    pass
-
-    def get_book(self, param, param1, param2, user):
-        pass
+    def get_book(self, author: str, book_name: str, days_to_return: int, user: User):
+        # the two conditions here per how the description is written are that the book is either rented, or not
+        # it doesnt look that the book is not exist
+        try:
+            found_book = [book_name for author, books in self.books_available.items() if book_name in books][0]
+        except IndexError:
+            return 's'
 
     def return_book(self, param, param1, user):
         pass
