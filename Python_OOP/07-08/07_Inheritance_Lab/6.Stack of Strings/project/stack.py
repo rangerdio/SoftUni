cfcec62e42b2715a8e39ddfb9aef3b64a588bb91
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-10-31 14:31:19 +0200

    py update

--- Python_OOP/07-08/07_Inheritance_Lab/6.Stack of Strings/project/stack.py
+++ Python_OOP/07-08/07_Inheritance_Lab/6.Stack of Strings/project/stack.py
@@ -1,34 +1,52 @@
 class Stack:
-    def __init__(self, data: list):
-        self.data = data
+    def __init__(self, *args):
+        self.data: list = []
 
-    def push(self, element) -> None:
+    def push(self, element: str) -> None:
         self.data.append(element)
 
     def pop(self) -> str:
         return self.data.pop()
 
     def top(self):
-        pass
+        return self.data[-1]
 
     def is_empty(self) -> bool:
         return len(self.data) == 0
 
     def __str__(self) -> str:
-        # return '[' + ', '.join(element for element in self.data) + ']'
-        return f'{self.data}'
+        return '[' + ', '.join(element for element in self.data[::-1]) + ']'
+        # return f'{self.data}'
 
 
-s = Stack([1, 22, 333])
-s.push(444)
-print(s)
-s.pop()
-print(s)
-s.pop()
-print(s)
-s.pop()
-print(s)
-print(s.is_empty())
-s.pop()
-print(s)
-print(s.is_empty())
+data = Stack()
+
+# data.push('1')
+# data.push('22')
+# data.push('333')
+# data.push('444')
+# print(data)
+# data.pop()
+# print(data)
+# data.pop()
+# print(data)
+# data.pop()
+# print(data)
+# print(data.is_empty())
+# data.pop()
+# print(data)
+# print(data.is_empty())
+
+data.push("apple")
+data.push("carrot")
+print(str(data))
+# self.assertEqual(str(data), '[carrot, apple]')
+# self.assertEqual(data.pop(), 'carrot')
+data.pop()
+print(data)
+# self.assertEqual(data.top(), 'apple')
+print(data.top(), 'apple')
+data.push("cucumber")
+print(data)
+# self.assertEqual(str(data), '[cucumber, apple]')
+# self.assertEqual(data.is_empty(), False)
