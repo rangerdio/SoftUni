f30056aa699a07176113ee2dec80d0526761c6d6
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-11-01 14:55:04 +0200

    0

--- Python_OOP/07-08_Inheritance/08_Inheritance_Exercises/4.Need for Speed/project/vehicle.py
+++ Python_OOP/07-08_Inheritance/08_Inheritance_Exercises/4.Need for Speed/project/vehicle.py
@@ -4,7 +4,7 @@ class Vehicle:
     def __init__(self, fuel: float, horse_power: int):
         self.fuel = fuel
         self.horse_power = horse_power
-        self.fuel_consumption: float = self.DEFAULT_FUEL_CONSUMPTION
+        self.fuel_consumption = self.DEFAULT_FUEL_CONSUMPTION
 
     def drive(self, kilometers: float):
         fuel_needed = kilometers * self.fuel_consumption
