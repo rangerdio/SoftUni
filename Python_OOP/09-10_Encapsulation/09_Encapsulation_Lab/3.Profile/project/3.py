44646d4e9d9f27cb548986a7633de9f7fbe51329
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-11-03 12:41:38 +0200

    update

---
+++ Python_OOP/09-10_Encapsulation/09_Encapsulation_Lab/3.Profile/project/3.py
@@ -0,0 +1,34 @@
+class Profile:
+    def __init__(self, username: str, password: str):
+        self.username = username
+        self.password = password
+
+    @property
+    def username(self):
+        return self.__username
+
+    @username.setter
+    def username(self, value):
+        if 5 > len(value) or len(value) > 15:
+            raise ValueError('The username must be between 5 and 15 characters.')
+        self.__username = value
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        if not (any(letter.isdigit() for letter in value) and
+                any(letter.isupper() for letter in value) and
+                len(value) >= 8):
+            raise ValueError('The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.')
+        self.__password = value
+
+    def __str__(self):
+        return f'You have a profile with username: "{self.username}" and password: {"*" * len(self.password)}'
+
+
+# correct_profile = Profile("Username", "Passw0rd")
+# print(correct_profile)
+#
