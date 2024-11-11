60185560823b4723d5763f7390eb3eed612e457b
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-11-11 14:06:29 +0200

    update

--- Python_OOP/11-12_Static-and-Class-Methods/12_Static-and-Class-Methods-Exercise/3.Document Management/project/storage.py
+++ Python_OOP/11-12_Static-and-Class-Methods/12_Static-and-Class-Methods-Exercise/3.Document Management/project/storage.py
@@ -10,49 +10,47 @@ class Storage:
         self.topics: list[Topic] = []
         self.documents: list[Document] = []
 
-    def add_category(self, category: Category):
+    def add_category(self, category: Category) -> None:
         self.categories.append(category) if category not in self.categories else None
 
-    def add_topic(self, topic: Topic):
+    def add_topic(self, topic: Topic) -> None:
         self.topics.append(topic) if topic not in self.topics else None
 
-    def add_document(self, document: Document):
+    def add_document(self, document: Document) -> None:
         self.documents.append(document) if document not in self.documents else None
 
-    def edit_category(self, category_id: int, new_name: str):
-        category = self.get_category(category_id)
-        category.name = new_name
+    def edit_category(self, category_id: int, new_name: str) -> None:
+        self.__edit_object(category_id, self.categories, new_name)
 
-    def edit_topic(self, topic_id: int, new_topic: str, new_storage_folder: str):
-        topic = self.get_topic(topic_id)
-        topic.topic, topic.storage_folder = new_topic, new_storage_folder
+    def edit_topic(self, topic_id: int, new_topic: str, new_storage_folder: str) -> None:
+        self.__edit_object(topic_id, self.topics, new_topic, new_storage_folder)
 
-    def edit_document(self, document_id: int, new_file_name: str):
-        document = self.get_document(document_id)
-        document.file_name = new_file_name
+    def edit_document(self, document_id: int, new_file_name: str) -> None:
+        self.__edit_object(document_id, self.documents, new_file_name)
 
-    def delete_category(self, category_id):
-        category = self.get_category(category_id)
-        self.categories.remove(category)
+    @staticmethod
+    def __get_object(object_id: int, object_list):
+        return [obj for obj in object_list if obj.id == object_id][0]
 
-    def delete_topic(self, topic_id):
-        topic = self.get_topic(topic_id)
-        self.topics.remove(topic)
+    def __edit_object(self, object_id, object_list, *values) -> None:
+        current_object = self.__get_object(object_id, object_list)
+        current_object.edit(*values)
 
-    def delete_document(self, document_id):
-        document = self.get_document(document_id)
-        self.documents.remove(document)
+    def __delete_object(self, object_id, object_list):
+        current_object = self.__get_object(object_id, object_list)
+        object_list.remove(current_object)
+
+    def delete_category(self, category_id) -> None:
+        self.__delete_object(category_id, self.categories)
+
+    def delete_topic(self, topic_id) -> None:
+        self.__delete_object(topic_id, self.topics)
+
+    def delete_document(self, document_id) -> None:
+        self.__delete_object(document_id, self.documents)
 
     def get_document(self, document_id):
-        return [doc for doc in self.documents if doc.id == document_id][0]
-
-    def get_category(self, category_id):
-        return [cat for cat in self.categories if cat.id == category_id][0]
-
-    def get_topic(self, topic_id):
-        return [top for top in self.topics if top.id == topic_id][0]
+        return self.__get_object(document_id, self.documents)
 
     def __repr__(self):
-        result = []
-        result.extend(repr(document) for document in self.documents)
-        return "\n".join(result)
+        return "\n".join(str(docu) for docu in self.documents)
