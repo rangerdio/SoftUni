77e6ff85a7cc06661b8295244ed047d1df79b4ff
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-11-27 18:43:22 +0200

    update

---
+++ Python_OOP/17-18_Iterators-and-Generators-Exercise/18_Iterators-and-Generators-Exercise/7.Reader/project/7.py
@@ -0,0 +1,12 @@
+from typing import Iterable
+
+
+def read_next(*iterable: Iterable):
+    for element in iterable:
+        yield from element
+
+
+# for item in read_next("string", (2,), {"d": 1, "i": 2, "c": 3, "t": 4}):
+#     print(item, end='')
+for i in read_next("Need", (2, 3), ["words", "."]):
+    print(i)
