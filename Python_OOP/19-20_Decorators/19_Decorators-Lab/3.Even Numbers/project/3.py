eeab38523a3a99d635eb783804601621332feb66
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-12-03 23:28:16 +0200

    update

--- Python_OOP/19-20_Decorators/19_Decorators-Lab/3.Even Numbers/project/3.py
+++ Python_OOP/19-20_Decorators/19_Decorators-Lab/3.Even Numbers/project/3.py
@@ -1,7 +1,13 @@
 def even_numbers(function):
 
     def wrapper(numbers):
-
-        # TODO: Implement
-
+        return [number for number in numbers if number % 2 == 0]
     return wrapper
+
+
+@even_numbers
+def get_numbers(numbers):
+    return numbers
+
+
+print(get_numbers([1, 2, 3, 4, 5]))
