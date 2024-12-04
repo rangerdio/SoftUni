b4028b60b9f8e70347d8bee6122742b415f59f4f
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-12-04 20:12:31 +0200

    update

---
+++ Python_OOP/19-20_Decorators/20_Decorators-Exercises/4.Type Check/project/4.py
@@ -0,0 +1,26 @@
+def type_check(type_):
+    def decorator(function):
+        def wrapper(*args):
+            if not isinstance(*args, type_):
+                return "Bad Type"
+            return function(*args)
+        return wrapper
+    return decorator
+
+
+@type_check(int)
+def times2(num):
+    return num * 2
+
+
+print(times2(2))
+print(times2('Not A Number'))
+
+
+@type_check(str)
+def first_letter(word):
+    return word[0]
+
+
+print(first_letter('Hello World'))
+print(first_letter(['Not', 'A', 'String']))
