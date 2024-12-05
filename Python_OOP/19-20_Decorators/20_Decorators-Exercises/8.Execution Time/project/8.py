2c715835d23d5943412272980916f3d0d70c9ad6
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-12-05 11:45:38 +0200

    update

---
+++ Python_OOP/19-20_Decorators/20_Decorators-Exercises/8.Execution Time/project/8.py
@@ -0,0 +1,36 @@
+def exec_time(function):
+    pass
+
+
+@exec_time
+def loop(start, end):
+    total = 0
+    for x in range(start, end):
+        total += x
+    return total
+
+
+print(loop(1, 10000000))
+print('=============================================')
+
+
+@exec_time
+def concatenate(strings):
+    result = ""
+    for string in strings:
+        result += string
+    return result
+
+
+print(concatenate(["a" for i in range(1000000)]))
+print('=============================================')
+
+
+@exec_time
+def loop():
+    count = 0
+    for i in range(1, 9999999):
+        count += 1
+
+
+print(loop())
