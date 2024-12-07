65564f9ae8ceb07e4bb373ceeeb233c4589039db
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-12-07 14:19:46 +0200

    00

--- Python_OOP/21-22_Testing/21_Testing-Lab/3.List/project/test.py
+++ Python_OOP/21-22_Testing/21_Testing-Lab/3.List/project/test.py
@@ -4,7 +4,58 @@ from unittest import TestCase, main
 
 
 class IntegerListTests(TestCase):
-    ...
+
+    def setUp(self):
+        self.nums_list = IntegerList(1, 2, 'as', [1, 1])
+
+    def test_init(self):
+        nums = IntegerList()
+        self.assertEqual(nums._IntegerList__data, [])
+
+        nums = IntegerList('koiko', 1.233, 33, [1, 2, 3], (1, 2, 3))
+        self.assertEqual(nums._IntegerList__data, [33])
+
+    def test_get_data(self):
+        result = self.nums_list.get_data()
+        self.assertEqual(result, [1, 2])
+
+    def test_add_data_raise_error(self):
+        self.nums_list.add(3)
+        self.assertEqual(self.nums_list._IntegerList__data, [1, 2, 3])
+
+        with self.assertRaises(ValueError) as err:
+            self.nums_list.add('koiko')
+        self.assertEqual(str(err.exception), "Element is not Integer")
+        self.assertEqual(self.nums_list._IntegerList__data, [1, 2, 3])
+
+    def test_add_with_integer(self):
+        self.assertEqual(self.nums_list.get_data(), [1, 2])
+        koko = self.nums_list.add(3)
+        self.assertEqual(koko, [1, 2, 3])
+        self.assertEqual(self.nums_list.get_data(), [1, 2, 3])
+
+    def test_remove_index_raise_exception(self):
+        self.assertEqual(len(self.nums_list.get_data()), 2)
+
+        with self.assertRaises(IndexError) as err:
+            self.nums_list.remove_index(2)
+        self.assertEqual(str(err.exception), "Index is out of range")
+        self.assertEqual(len(self.nums_list.get_data()), 2)
+
+        with self.assertRaises(IndexError) as err:
+            self.nums_list.remove_index(3)
+        self.assertEqual(str(err.exception), "Index is out of range")
+        self.assertEqual(len(self.nums_list.get_data()), 2)
+
+    def test_remove_valid_index(self):
+        value = self.nums_list.get_data()[1]
+        self.assertEqual(value, 2)
+        self.assertEqual(len(self.nums_list.get_data()), 2)
+
+        ss = self.nums_list.remove_index(1)
+        self.assertEqual(self.nums_list.get_data(), [1])
+        self.assertEqual(ss, value)
+        self.assertEqual(len(self.nums_list.get_data()), 1)
 
 
 if __name__ == '__main__':
