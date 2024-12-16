e1f84ffea47aa30d901e0e64113826c237c994c8
rangerdio <148572548+rangerdio@users.noreply.github.com>
2024-12-16 11:34:53 +0200

    00

--- Python_OOP/24_Exam_prep_1/1-2/project/products/base_product.py
+++ Python_OOP/24_Exam_prep_1/1-2/project/products/base_product.py
@@ -3,14 +3,50 @@ from abc import ABC, abstractmethod
 
 class BaseProduct(ABC):
     def __init__(self, model: str, price: float, material: str, sub_type: str):
-        if len(model.strip()) < 3:
-            raise ValueError("Product model must be at least 3 chars long!")
-        if price <= 0.0:
-            raise ValueError("Product price must be greater than zero!")
         self.model = model
         self.price = price
         self.material = material
         self.sub_type = sub_type
+
+    @property
+    def model(self):
+        return self._model
+
+    @model.setter
+    def model(self, value: str):
+        if len(value.strip()) < 3:
+            raise ValueError("Product model must be at least 3 chars long!")
+        self._model = value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value: float):
+        if value <= 0.0:
+            raise ValueError("Product price must be greater than zero!")
+        self._price = value
+
+    @property
+    def material(self):
+        return self._material
+
+    @material.setter
+    def material(self, value: str):
+        if not value.strip():
+            raise ValueError("Product material cannot be empty!")
+        self._material = value
+
+    @property
+    def sub_type(self):
+        return self._sub_type
+
+    @sub_type.setter
+    def sub_type(self, value: str):
+        if not value.strip():
+            raise ValueError("Product sub-type cannot be empty!")
+        self._sub_type = value
 
     @abstractmethod
     def discount(self):
